b88ffd8b74af814ce168691cc90ff438c9abae40
Matus Chochlik <chochlik@gmail.com>
2015-09-30 22:26:41 +0200

    Added member_function_constant and its tests

---
+++ include/eagine/mem_func_const.hpp
@@ -0,0 +1,76 @@
+/**
+ *  @file eagine/mem_func_const.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#ifndef EAGINE_MEM_FUNC_CONST_1509260923_HPP
+#define EAGINE_MEM_FUNC_CONST_1509260923_HPP
+
+#include <utility>
+
+namespace eagine {
+
+template <typename T, T Ptr>
+struct member_function_constant;
+
+template <typename RV, typename C, typename ... P, RV(C::*Ptr)(P...)>
+struct member_function_constant<RV(C::*)(P...), Ptr>
+{
+	typedef RV(C::*pointer)(P...);
+	typedef RV(*free_pointer)(C*, P...);
+	typedef RV result_type;
+	typedef C scope;
+
+	static constexpr
+	pointer get(void)
+	noexcept
+	{
+		return Ptr;
+	}
+
+	static RV free_func(C* c, P ... a)
+	{
+		return (c->*Ptr)(std::forward<P>(a)...);
+	}
+
+	static free_pointer make_free(void)
+	noexcept
+	{
+		return &free_func;
+	}
+};
+
+template <typename RV, typename C, typename ... P, RV(C::*Ptr)(P...) const>
+struct member_function_constant<RV(C::*)(P...) const, Ptr>
+{
+	typedef RV(C::*pointer)(P...) const;
+	typedef RV(*free_pointer)(const C*, P...);
+	typedef RV result_type;
+	typedef C scope;
+
+	static constexpr
+	pointer get(void)
+	noexcept
+	{
+		return Ptr;
+	}
+
+	static RV free_func(const C* c, P ... a)
+	{
+		return (c->*Ptr)(std::forward<P>(a)...);
+	}
+
+	static free_pointer make_free(void)
+	noexcept
+	{
+		return &free_func;
+	}
+};
+
+} // namespace eagine
+
+#endif // include guard
---
+++ test/eagine/mem_func_const_r.cpp
@@ -0,0 +1,103 @@
+/*
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE EAGINE_mem_func_const
+#include <boost/test/unit_test.hpp>
+
+#include <eagine/mem_func_const.hpp>
+#include <type_traits>
+
+struct bar
+{
+	int i;
+
+	int foo(int j) const
+	{
+		return i+j;
+	}
+
+	float foo(int j, float k)
+	{
+		return (i+j)/k;
+	}
+};
+
+struct baz
+{
+	int i;
+
+	baz& inc(void)
+	{
+		++i;
+		return *this;
+	}
+};
+
+BOOST_AUTO_TEST_SUITE(mem_func_const)
+
+BOOST_AUTO_TEST_CASE(mem_func_const_pointer_get)
+{
+	using namespace eagine;
+
+	typedef member_function_constant<int(bar::*)(int) const, &bar::foo> fubar1;
+	typedef member_function_constant<float(bar::*)(int,float), &bar::foo> fubar2;
+	typedef member_function_constant<baz&(baz::*)(void), &baz::inc> incbaz;
+
+	fubar1::pointer pfubar1 = fubar1::get();
+	fubar2::pointer pfubar2 = fubar2::get();
+	incbaz::pointer pincbaz = incbaz::get();
+
+	bar br{432};
+
+	BOOST_CHECK_EQUAL(((br).*(pfubar1))(543), 432+543);
+	BOOST_CHECK_EQUAL(((br).*(pfubar2))(543, 654.3f), (432+543)/654.3f);
+
+	baz bz{1};
+
+	BOOST_CHECK_EQUAL(bz.i, 1);
+	((bz).*(pincbaz))();
+	BOOST_CHECK_EQUAL(bz.i, 2);
+
+}
+
+BOOST_AUTO_TEST_CASE(mem_func_const_scope)
+{
+	using namespace eagine;
+
+	typedef member_function_constant<int(bar::*)(int) const, &bar::foo> fubar1;
+	typedef member_function_constant<float(bar::*)(int,float), &bar::foo> fubar2;
+	typedef member_function_constant<baz&(baz::*)(void), &baz::inc> incbaz;
+
+	BOOST_CHECK(( std::is_same<bar, fubar1::scope>::value));
+	BOOST_CHECK(( std::is_same<bar, fubar2::scope>::value));
+	BOOST_CHECK((!std::is_same<baz, fubar1::scope>::value));
+	BOOST_CHECK((!std::is_same<baz, fubar2::scope>::value));
+	BOOST_CHECK((!std::is_same<bar, incbaz::scope>::value));
+	BOOST_CHECK(( std::is_same<baz, incbaz::scope>::value));
+}
+
+BOOST_AUTO_TEST_CASE(mem_func_const_make_free)
+{
+	using namespace eagine;
+
+	typedef member_function_constant<int(bar::*)(int) const, &bar::foo> fubar1;
+	typedef member_function_constant<float(bar::*)(int,float), &bar::foo> fubar2;
+	typedef member_function_constant<baz&(baz::*)(void), &baz::inc> incbaz;
+
+	bar br{123};
+
+	BOOST_CHECK_EQUAL(fubar1::make_free()(&br, 234), 123+234);
+	BOOST_CHECK_EQUAL(fubar2::make_free()(&br, 234, 345.6f), (123+234)/345.6f);
+
+	baz bz{1};
+	BOOST_CHECK_EQUAL(bz.i, 1);
+	incbaz::make_free()(&bz);
+	BOOST_CHECK_EQUAL(bz.i, 2);
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
