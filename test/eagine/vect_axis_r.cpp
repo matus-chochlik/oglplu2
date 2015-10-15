c779587f6b35ac74cc6a2b42f33394b6d4a75022
Matus Chochlik <chochlik@gmail.com>
2015-10-15 20:20:16 +0200

    Added new tests for the vectorization utils

---
+++ test/eagine/vect_axis_r.cpp
@@ -0,0 +1,101 @@
+/**
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE EAGINE_vect_axis
+#include <boost/test/unit_test.hpp>
+
+#include <eagine/vect/axis.hpp>
+#include <cstdlib>
+
+BOOST_AUTO_TEST_SUITE(vect_axis)
+
+template <typename T, unsigned I, unsigned N, bool V>
+void test_vect_axis_apply_TINV(void)
+{
+	for(unsigned k=0; k<1000; ++k)
+	{
+		T a = std::rand();
+
+		typename eagine::vect::data<T, N, V>::type v =
+			eagine::vect::axis<T, N, I, V>::apply(a);
+
+		for(unsigned i=0; i<N; ++i)
+		{
+			BOOST_CHECK_EQUAL(v[i], T(i==I?a:0));
+		}
+	}
+}
+
+template <typename T, bool V>
+void test_vect_axis_apply_TV(void)
+{
+	test_vect_axis_apply_TINV<T, 0, 2, V>();
+	test_vect_axis_apply_TINV<T, 1, 2, V>();
+	test_vect_axis_apply_TINV<T, 2, 2, V>();
+
+	test_vect_axis_apply_TINV<T, 0, 3, V>();
+	test_vect_axis_apply_TINV<T, 1, 3, V>();
+	test_vect_axis_apply_TINV<T, 2, 3, V>();
+	test_vect_axis_apply_TINV<T, 3, 3, V>();
+
+	test_vect_axis_apply_TINV<T, 0, 4, V>();
+	test_vect_axis_apply_TINV<T, 1, 4, V>();
+	test_vect_axis_apply_TINV<T, 2, 4, V>();
+	test_vect_axis_apply_TINV<T, 3, 4, V>();
+	test_vect_axis_apply_TINV<T, 4, 4, V>();
+
+	test_vect_axis_apply_TINV<T, 0, 5, V>();
+	test_vect_axis_apply_TINV<T, 1, 5, V>();
+	test_vect_axis_apply_TINV<T, 2, 5, V>();
+	test_vect_axis_apply_TINV<T, 3, 5, V>();
+	test_vect_axis_apply_TINV<T, 4, 5, V>();
+	test_vect_axis_apply_TINV<T, 5, 5, V>();
+
+	test_vect_axis_apply_TINV<T, 0, 6, V>();
+	test_vect_axis_apply_TINV<T, 1, 6, V>();
+	test_vect_axis_apply_TINV<T, 2, 6, V>();
+	test_vect_axis_apply_TINV<T, 3, 6, V>();
+	test_vect_axis_apply_TINV<T, 4, 6, V>();
+	test_vect_axis_apply_TINV<T, 5, 6, V>();
+	test_vect_axis_apply_TINV<T, 6, 6, V>();
+
+	test_vect_axis_apply_TINV<T, 0, 7, V>();
+	test_vect_axis_apply_TINV<T, 1, 7, V>();
+	test_vect_axis_apply_TINV<T, 2, 7, V>();
+	test_vect_axis_apply_TINV<T, 3, 7, V>();
+	test_vect_axis_apply_TINV<T, 4, 7, V>();
+	test_vect_axis_apply_TINV<T, 5, 7, V>();
+	test_vect_axis_apply_TINV<T, 6, 7, V>();
+	test_vect_axis_apply_TINV<T, 7, 7, V>();
+
+	test_vect_axis_apply_TINV<T, 0, 8, V>();
+	test_vect_axis_apply_TINV<T, 1, 8, V>();
+	test_vect_axis_apply_TINV<T, 2, 8, V>();
+	test_vect_axis_apply_TINV<T, 3, 8, V>();
+	test_vect_axis_apply_TINV<T, 4, 8, V>();
+	test_vect_axis_apply_TINV<T, 5, 8, V>();
+	test_vect_axis_apply_TINV<T, 6, 8, V>();
+	test_vect_axis_apply_TINV<T, 7, 8, V>();
+	test_vect_axis_apply_TINV<T, 8, 8, V>();
+}
+
+template <typename T>
+void test_vect_axis_apply_T(void)
+{
+	test_vect_axis_apply_TV<T, true>();
+	test_vect_axis_apply_TV<T,false>();
+}
+
+BOOST_AUTO_TEST_CASE(vect_axis_apply)
+{
+	test_vect_axis_apply_T<int>();
+	test_vect_axis_apply_T<float>();
+	test_vect_axis_apply_T<double>();
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ test/eagine/vect_hsum_r.cpp
@@ -0,0 +1,80 @@
+/**
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE eagine_vect_hsum
+#include <boost/test/unit_test.hpp>
+
+#include <eagine/vect/hsum.hpp>
+#include <cstdlib>
+
+BOOST_AUTO_TEST_SUITE(vect_hsum)
+
+template <typename T, unsigned N, bool V>
+void test_vect_hsum_TNV(void)
+{
+	for(unsigned k=0; k<10; ++k)
+	{
+		T s = T(0);
+
+		typename eagine::vect::data<T, N, V>::type v = {};
+
+		for(unsigned i=0; i<N; ++i)
+		{
+			T n = std::rand() / T(2);
+			v[i] = n;
+			s += n;
+		}
+
+		typename eagine::vect::data<T, N, V>::type r =
+			eagine::vect::hsum<T, N, V>::apply(v);
+
+		for(unsigned i=0; i<N; ++i)
+		{
+			BOOST_CHECK_EQUAL(s, r[i]);
+		}
+	}
+}
+
+template <typename T, bool V>
+void test_vect_hsum_TV(void)
+{
+	test_vect_hsum_TNV<T, 2, V>();
+	test_vect_hsum_TNV<T, 3, V>();
+	test_vect_hsum_TNV<T, 4, V>();
+	test_vect_hsum_TNV<T, 5, V>();
+	test_vect_hsum_TNV<T, 6, V>();
+	test_vect_hsum_TNV<T, 7, V>();
+	test_vect_hsum_TNV<T, 8, V>();
+
+	test_vect_hsum_TNV<T, 11, V>();
+	test_vect_hsum_TNV<T, 17, V>();
+	test_vect_hsum_TNV<T, 23, V>();
+}
+
+template <typename T>
+void test_vect_hsum_T(void)
+{
+	test_vect_hsum_TV<T, true>();
+	test_vect_hsum_TV<T,false>();
+}
+
+BOOST_AUTO_TEST_CASE(vect_hsum_apply_int)
+{
+	test_vect_hsum_T<int>();
+}
+
+BOOST_AUTO_TEST_CASE(vect_hsum_apply_float)
+{
+	test_vect_hsum_T<float>();
+}
+
+BOOST_AUTO_TEST_CASE(vect_hsum_apply_double)
+{
+	test_vect_hsum_T<double>();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
