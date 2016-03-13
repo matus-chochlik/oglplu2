607d6f7bbeb65ff946d7bf5644cfa6da716c7436
Matus Chochlik <chochlik@gmail.com>
2016-03-13 08:11:34 +0100

    Added new range algorighm tests

--- test/eagine/range_algo_r.cpp
+++ test/eagine/range_algo_r.cpp
@@ -675,6 +675,139 @@ BOOST_AUTO_TEST_CASE(range_algo_find_1)
 	test_range_find_1<int>(-100000, 100000);
 }
 
+template <typename T>
+void test_range_strip_prefix_1(eagine::span<T> rng1, eagine::span<T> rng2)
+{
+	using namespace eagine;
+
+	bool had_prefix = ranges::starts_with(rng1, rng2);
+
+	eagine::span<T> rng3 = ranges::strip_prefix(rng1, rng2);
+
+	bool has_prefix = ranges::starts_with(rng3, rng2);
+
+	if(had_prefix)
+	{
+		BOOST_CHECK(!has_prefix);
+		BOOST_CHECK_EQUAL(rng1.size(), rng2.size()+rng3.size());
+	}
+	else
+	{
+		BOOST_CHECK(!has_prefix);
+		BOOST_CHECK_EQUAL(rng1.size(), rng3.size());
+	}
+}
+
+template <typename T>
+void test_range_strip_prefix_1(T min, T max, bool has)
+{
+	std::vector<T> v1(20+std::rand()%80);
+	std::vector<T> v2(std::size_t(std::rand()%int(v1.size())));
+
+	typename std::vector<T>::size_type i = 0;
+
+	while(i < v2.size())
+	{
+		v1[i] = T(min + (std::rand()%(max-min)));
+		if(has) v2[i] = v1[i];
+		else v2[i] = T(min + (std::rand()%(max-min)));
+		++i;
+	}
+
+	while(i < v1.size())
+	{
+		v1[i] = T(min + (std::rand()%(max-min)));
+		++i;
+	}
+
+	test_range_strip_prefix_1(eagine::as_span(v1), eagine::as_span(v2));
+}
+
+template <typename T>
+void test_range_strip_prefix_1(T min, T max)
+{
+	test_range_strip_prefix_1(min, max, true);
+	test_range_strip_prefix_1(min, max,false);
+
+	for(int i=0; i<10; ++i)
+	{
+		test_range_strip_prefix_1(min, max, std::rand()%2==0);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(range_algo_strip_prefix_1)
+{
+	test_range_strip_prefix_1<char>('A','Z');
+	test_range_strip_prefix_1<int>(-100000, 100000);
+}
+
+template <typename T>
+void test_range_strip_suffix_1(eagine::span<T> rng1, eagine::span<T> rng2)
+{
+	using namespace eagine;
+
+	bool had_suffix = ranges::ends_with(rng1, rng2);
+
+	eagine::span<T> rng3 = ranges::strip_suffix(rng1, rng2);
+
+	bool has_suffix = ranges::ends_with(rng3, rng2);
+
+	if(had_suffix)
+	{
+		BOOST_CHECK(!has_suffix);
+		BOOST_CHECK_EQUAL(rng1.size(), rng2.size()+rng3.size());
+	}
+	else
+	{
+		BOOST_CHECK(!has_suffix);
+		BOOST_CHECK_EQUAL(rng1.size(), rng3.size());
+	}
+}
+
+template <typename T>
+void test_range_strip_suffix_1(T min, T max, bool has)
+{
+	std::vector<T> v1(20+std::rand()%80);
+	std::vector<T> v2(std::size_t(std::rand()%int(v1.size())));
+
+	typename std::vector<T>::size_type i = 0;
+	typename std::vector<T>::size_type p = v1.size() - v2.size();
+
+	while(i < p)
+	{
+		v1[i] = T(min + (std::rand()%(max-min)));
+		++i;
+	}
+
+	while(i < v1.size())
+	{
+		v1[i] = T(min + (std::rand()%(max-min)));
+		if(has) v2[i-p] = v1[i];
+		else v2[i-p] = T(min + (std::rand()%(max-min)));
+		++i;
+	}
+
+	test_range_strip_suffix_1(eagine::as_span(v1), eagine::as_span(v2));
+}
+
+template <typename T>
+void test_range_strip_suffix_1(T min, T max)
+{
+	test_range_strip_suffix_1(min, max, true);
+	test_range_strip_suffix_1(min, max,false);
+
+	for(int i=0; i<10; ++i)
+	{
+		test_range_strip_suffix_1(min, max, std::rand()%2==0);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(range_algo_strip_suffix_1)
+{
+	test_range_strip_suffix_1<char>('A','Z');
+	test_range_strip_suffix_1<int>(-100000, 100000);
+}
+
 // TODO
 
 BOOST_AUTO_TEST_SUITE_END()
