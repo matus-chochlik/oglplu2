53a1f3d328ec80ec784975fbec497e5737001614
Matus Chochlik <chochlik@gmail.com>
2016-05-07 19:36:00 +0200

    Added the eagine library sources and build scripts

---
+++ source/lib/eagine/random.cpp
@@ -0,0 +1,15 @@
+/**
+ *  .file lib/eagine/random.cpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#include "prologue.inl"
+
+#include "implement.inl"
+#include <eagine/random_bytes.hpp>
+#include "epilogue.inl"
+
