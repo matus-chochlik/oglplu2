4dab8be1063fc6fe96c3b9333a15bcb3d44dcae2
Matus Chochlik <chochlik@gmail.com>
2016-05-10 07:24:59 +0200

    Added eglplus library sources and build script

---
+++ include/eglplus/egl.hpp
@@ -0,0 +1,15 @@
+/**
+ *  @file eglplus/egl.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#ifndef OALPLUS_EGL_1509260923_HPP
+#define OALPLUS_EGL_1509260923_HPP
+
+#include <EGL/egl.h>
+// TODO
+
+#endif // include guard
