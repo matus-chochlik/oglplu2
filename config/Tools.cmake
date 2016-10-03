a5976a6c799e31ad07949a0f1e2bb869b18d9206
Matus Chochlik <chochlik@gmail.com>
2016-10-03 13:00:22 +0200

    Updated the buildsystem tools

--- config/Tools.cmake
+++ config/Tools.cmake
@@ -7,11 +7,15 @@ file(WRITE "${PROJECT_BINARY_DIR}/SOURCE_DIR" "${PROJECT_SOURCE_DIR}")
 file(WRITE "${PROJECT_SOURCE_DIR}/BINARY_DIR" "${PROJECT_BINARY_DIR}")
 
 configure_file(
-	"${PROJECT_SOURCE_DIR}/config/tools/complete-run_test"
+	"${PROJECT_SOURCE_DIR}/config/tools/bash-complete"
 	"${PROJECT_BINARY_DIR}/" COPYONLY
 )
 configure_file(
 	"${PROJECT_SOURCE_DIR}/config/tools/run_test-oglplus"
+	"${PROJECT_BINARY_DIR}/" COPYONLY
+)
+configure_file(
+	"${PROJECT_SOURCE_DIR}/config/tools/run_example-oglplus"
 	"${PROJECT_BINARY_DIR}/" COPYONLY
 )
 configure_file(
