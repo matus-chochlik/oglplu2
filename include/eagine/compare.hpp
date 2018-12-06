48daf0cdc628df7f896e02b31850fddd1d8f1380
Matus Chochlik <chochlik@gmail.com>
2018-12-06 08:04:16 +0100

    Added the compare utility

---
+++ include/eagine/compare.hpp
@@ -0,0 +1,83 @@
+/**
+ *  @file eagine/compare.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#include "identity.hpp"
+#include "std/type_traits.hpp"
+#include <limits>
+#include <utility>
+
+#ifndef EAGINE_COMPARE_HPP
+#define EAGINE_COMPARE_HPP
+
+namespace eagine {
+//------------------------------------------------------------------------------
+template <typename L, typename R>
+struct equal_cmp_any {
+    static constexpr inline bool check(const L& l, const R& r) noexcept {
+        return l == r;
+    }
+};
+//------------------------------------------------------------------------------
+template <typename L, typename R, bool LSign, bool RSign>
+struct equal_cmp_int;
+//------------------------------------------------------------------------------
+template <typename L, typename R, bool Sign>
+struct equal_cmp_int<L, R, Sign, Sign> {
+    static constexpr inline bool check(L l, R r) noexcept {
+        return l == r;
+    }
+};
+//------------------------------------------------------------------------------
+template <typename L, typename R>
+struct equal_cmp_int<L, R, true, false> {
+    static constexpr inline bool check(L l, R r) noexcept {
+        using Tmp = std::make_unsigned_t<L>;
+        return (l < 0) ? false : Tmp(l) == r;
+    }
+};
+//------------------------------------------------------------------------------
+template <typename L, typename R>
+struct equal_cmp_int<L, R, false, true> : equal_cmp_int<R, L, true, false> {};
+//------------------------------------------------------------------------------
+template <typename L, typename R>
+using equal_cmp_pick = std::conditional_t<
+  std::is_integral_v<L> && std::is_integral_v<R>,
+  equal_cmp_int<L, R, std::is_signed_v<L>, std::is_signed_v<R>>,
+  equal_cmp_any<L, R>>;
+//------------------------------------------------------------------------------
+template <typename L, typename R>
+struct equal_cmp : equal_cmp_pick<L, R> {};
+//------------------------------------------------------------------------------
+template <typename L, typename R>
+struct equal_cmp_obj : equal_cmp<L, R> {
+    constexpr inline equal_cmp_obj(L l, R r) noexcept
+      : _l{std::move(l)}
+      , _r{std::move(r)} {
+    }
+
+    constexpr explicit inline operator bool() const noexcept {
+        return equal_cmp<L, R>::check(_l, _r);
+    }
+
+    L _l = {};
+    R _r = {};
+};
+//------------------------------------------------------------------------------
+template <typename T>
+struct cmp_decay_to : identity<T> {};
+template <typename T>
+using cmp_decay_to_t = typename cmp_decay_to<T>::type;
+//------------------------------------------------------------------------------
+template <typename L, typename R>
+static constexpr inline bool are_equal(const L& l, const R& r) noexcept {
+    return equal_cmp<cmp_decay_to_t<L>, cmp_decay_to_t<R>>::check(l, r);
+}
+//------------------------------------------------------------------------------
+} // namespace eagine
+
+#endif // EAGINE_COMPARE_HPP
