2f4ec0cf2eb0ba8d81acaf658cb6f7b80a40f040
Matus Chochlik <chochlik@gmail.com>
2018-12-17 20:55:58 +0100

    Added / updated math functions

--- include/eagine/math/functions.hpp
+++ include/eagine/math/functions.hpp
@@ -10,47 +10,107 @@
 #define EAGINE_MATH_FUNCTIONS_1509260923_HPP
 
 #include "../std/type_traits.hpp"
+#include "constants.hpp"
 #include <cassert>
 #include <cmath>
 
 namespace eagine {
 namespace math {
-
-// minimum
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline auto sign(T x) noexcept {
+    return (x < 0) ? T(-1) : T(1);
+}
+//------------------------------------------------------------------------------
 template <typename T>
 static constexpr inline T minimum(T a, T b) noexcept {
     return a < b ? a : b;
 }
-
+//------------------------------------------------------------------------------
 template <typename T, typename... P>
 static constexpr inline T minimum(T a, T b, T c, P... d) noexcept {
     return minimum(minimum(a, b), c, d...);
 }
-
-// maximum
+//------------------------------------------------------------------------------
 template <typename T>
 static constexpr inline T maximum(T a, T b) noexcept {
     return a > b ? a : b;
 }
-
+//------------------------------------------------------------------------------
 template <typename T, typename... P>
 static constexpr inline T maximum(T a, T b, T c, P... d) noexcept {
     return maximum(maximum(a, b), c, d...);
 }
-
-// factorial
-static constexpr inline int factorial(int n) noexcept {
+//------------------------------------------------------------------------------
+template <typename T, typename Min, typename Max>
+static constexpr inline auto clamp(T x, Min min, Max max) noexcept {
+    return x < T(min) ? T(min) : x > T(max) ? T(max) : x;
+}
+//------------------------------------------------------------------------------
+template <typename T, typename S, typename E>
+static constexpr inline auto ramp(T x, S start, E end) noexcept {
+    return (clamp(x, start, end) - start) / (end - start);
+}
+//------------------------------------------------------------------------------
+template <typename T, typename A>
+static constexpr inline auto blend(T v1, T v2, A alpha) noexcept {
+    return v1 * alpha + v2 * (1 - alpha);
+}
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline auto inverse_logistic(T x) noexcept {
+    using std::log;
+    return log(x) - log(1 - x);
+}
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline auto logistic(T x) noexcept {
+    using std::exp;
+    return 1 / (1 + exp(-x));
+}
+//------------------------------------------------------------------------------
+template <typename T, typename C>
+static constexpr inline auto sigmoid01(T x, C c) noexcept {
+    return logistic(c * invLogistic(x));
+}
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline auto sigmoid01(T x) noexcept {
+    return sigmoid01(x, 2);
+}
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline auto sine_wave01(T x) noexcept {
+    using std::sin;
+    return (sin(2 * pi * x) + 1) / 2;
+}
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline auto cosine_wave01(T x) noexcept {
+    using std::cos;
+    return (cos(2 * pi * x) + 1) / 2;
+}
+//------------------------------------------------------------------------------
+template <typename T, typename U = T>
+static constexpr inline auto saw(T x, U u = T(1)) noexcept {
+    using std::fmod;
+    return fmod(x, u);
+}
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline std::enable_if_t<std::is_integral_v<T>, T> factorial(
+  T n) noexcept {
     return n > 0 ? n * factorial(n - 1) : 1;
 }
-
-// binomial
-static constexpr inline int binomial(int n, int k) noexcept {
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline std::enable_if_t<std::is_integral_v<T>, T> binomial(
+  T n, T k) noexcept {
     return ((n >= 0) && (k >= 0) && (k <= n))
              ? (factorial(n) / (factorial(k) * factorial(n - k)))
              : 0;
 }
-
-// bezier
+//------------------------------------------------------------------------------
 template <typename T, int N>
 struct bezier_t {
 private:
@@ -74,12 +134,12 @@ public:
         return _calc(N - 1, 0, t, p...);
     }
 };
-
+//------------------------------------------------------------------------------
 template <typename T, typename... P>
 static constexpr inline T bezier(T t, P... p) noexcept {
     return bezier_t<T, sizeof...(P)>()(t, p...);
 }
-
+//------------------------------------------------------------------------------
 } // namespace math
 } // namespace eagine
 
