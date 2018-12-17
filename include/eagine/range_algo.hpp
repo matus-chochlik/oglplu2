704b0d83f7f3720d1a2ed54556ae137c0941c3a1
Matus Chochlik <chochlik@gmail.com>
2018-12-17 21:06:03 +0100

    Removed old range algos

--- include/eagine/range_algo.hpp
+++ include/eagine/range_algo.hpp
@@ -19,137 +19,12 @@
 namespace eagine {
 namespace ranges {
 //------------------------------------------------------------------------------
-template <typename Range>
-static constexpr inline Range subrange(
-  const Range& rng,
-  valid_range_position<Range> bgn,
-  valid_range_position<Range> end) {
-    return {rng.data() + bgn.value(rng), end.value(rng) - bgn.value(rng)};
-}
-//------------------------------------------------------------------------------
-template <typename Range>
-static inline Range slice(
-  const Range& rng,
-  any_range_position<Range> pos,
-  any_range_position<Range> len) {
-    if(pos.value() > rng.size()) {
-        pos = rng.size();
-    }
-    if(len.value() > rng.size() - pos.value()) {
-        len = rng.size() - pos.value();
-    }
-    return subrange<Range>(rng, pos.value(), pos.value() + len.value());
-}
-//------------------------------------------------------------------------------
-template <typename Range>
-static inline Range slice(const Range& rng, any_range_position<Range> pos) {
-    if(pos.value() > rng.size()) {
-        pos = rng.size();
-    }
-    return subrange<Range>(rng, pos.value(), rng.size());
-}
-//------------------------------------------------------------------------------
-template <typename Range>
-static inline Range head(const Range& rng, any_range_position<Range> len) {
-    return slice(rng, 0, len);
-}
-//------------------------------------------------------------------------------
-template <typename Range>
-static inline Range tail(const Range& rng, any_range_position<Range> len) {
-    if(len.value() > rng.size()) {
-        len = rng.size();
-    }
-    return slice(rng, rng.size() - len.value(), len);
-}
-//------------------------------------------------------------------------------
-template <typename Range1, typename Range2>
-static inline bool starts_with(const Range1& rng, const Range2& with) {
-    return are_equal(head(rng, with.size()), with);
-}
-//------------------------------------------------------------------------------
-template <typename Range1, typename Range2>
-static inline bool ends_with(const Range1& rng, const Range2& with) {
-    return are_equal(tail(rng, with.size()), with);
-}
-//------------------------------------------------------------------------------
-template <typename Range1, typename Range2>
-static inline optionally_valid<range_index_t<Range1>>
-find_pos(const Range1& where, const Range2& what) {
-    const auto lt = what.size();
-    if(lt > 0) {
-        const auto ls = where.size();
-        if(ls > 0) {
-
-            if(ls >= lt) {
-                range_index_t<Range1> p = 0;
-                const range_index_t<Range1> n = ls - lt + 1;
-
-                while(p != n) {
-                    if(are_equal(slice(where, p, lt), what)) {
-                        return {p, true};
-                    }
-                    ++p;
-                }
-            }
-        }
-        return {ls, false};
-    }
-    return {0, true};
-}
-//------------------------------------------------------------------------------
-template <typename Range1, typename Range2>
-static inline optionally_valid<range_index_t<Range1>>
-rfind_pos(const Range1& where, const Range2& what) {
-    const auto lt = what.size();
-    if(lt > 0) {
-        const auto ls = where.size();
-        if(ls > 0) {
-
-            if(ls >= lt) {
-                range_index_t<Range1> p = ls - lt;
-
-                while(p >= 0) {
-                    if(are_equal(slice(where, p, lt), what)) {
-                        return {p, true};
-                    }
-                    --p;
-                }
-            }
-        }
-        return {ls, false};
-    }
-    return {0, true};
-}
-//------------------------------------------------------------------------------
-template <typename Range1, typename Range2>
-static inline bool contains(const Range1& where, const Range2& what) {
-    return find_pos(where, what).is_valid();
-}
-//------------------------------------------------------------------------------
 template <typename Range1, typename Range2>
 static inline Range1 find(const Range1& where, const Range2& what) {
     if(auto pos = find_pos(where, what)) {
         return slice(where, pos.value());
     }
     return {};
-}
-//------------------------------------------------------------------------------
-template <typename Range1, typename Range2>
-static inline Range1 strip_prefix(const Range1& rng, const Range2& prefix) {
-    range_index_t<Range1> ofs = 0;
-    if(starts_with(rng, prefix)) {
-        ofs = prefix.size();
-    }
-    return slice(rng, ofs);
-}
-//------------------------------------------------------------------------------
-template <typename Range1, typename Range2>
-static inline Range1 strip_suffix(const Range1& rng, const Range2& suffix) {
-    range_index_t<Range1> ofs = rng.size();
-    if(ends_with(rng, suffix)) {
-        ofs = ofs - suffix.size();
-    }
-    return slice(rng, 0, ofs);
 }
 //------------------------------------------------------------------------------
 template <typename Range1, typename Range2>
@@ -179,8 +54,8 @@ static inline Range1 slice_after_last(const Range1& rng, const Range2& what) {
 }
 //------------------------------------------------------------------------------
 template <typename Range1, typename Range2, typename Range3>
-static inline Range1
-slice_between(const Range1& rng, const Range2& bgn, const Range3& end) {
+static inline Range1 slice_between(
+  const Range1& rng, const Range2& bgn, const Range3& end) {
     if(auto bpos = find_pos(rng, bgn)) {
         if(auto epos = find_pos(slice(rng, bpos.value() + bgn.size()), end)) {
             return slice(rng, bpos.value() + bgn.size(), epos.value());
@@ -190,8 +65,8 @@ slice_between(const Range1& rng, const Range2& bgn, const Range3& end) {
 }
 //------------------------------------------------------------------------------
 template <typename Range1, typename Range2, typename Range3>
-static inline Range1
-slice_inside(const Range1& rng, const Range2& bgn, const Range3& end) {
+static inline Range1 slice_inside(
+  const Range1& rng, const Range2& bgn, const Range3& end) {
     if(auto bpos = find_pos(rng, bgn)) {
         if(auto epos = rfind_pos(rng, end)) {
             if(bpos.value() + bgn.size() < epos.value()) {
@@ -248,8 +123,8 @@ static inline BinaryOperation for_each_delimiter(
 }
 //------------------------------------------------------------------------------
 template <typename Range1, typename Range2>
-static inline span_size_t
-count_delimited(const Range1& where, const Range2& delim) {
+static inline span_size_t count_delimited(
+  const Range1& where, const Range2& delim) {
     return for_each_delimited(where, delim, count_t<span_size_t>());
 }
 //------------------------------------------------------------------------------
@@ -282,15 +157,15 @@ template <
   typename Range1,
   typename Range2,
   typename Transform>
-static inline BackInsertionSequence
-split(const Range1& rng, const Range2& delim, Transform transform) {
+static inline BackInsertionSequence split(
+  const Range1& rng, const Range2& delim, Transform transform) {
     BackInsertionSequence result;
     return std::move(split_into(result, rng, delim, transform));
 }
 //------------------------------------------------------------------------------
 template <typename BackInsertionSequence, typename Range1, typename Range2>
-static inline BackInsertionSequence
-split(const Range1& rng, const Range2& delim) {
+static inline BackInsertionSequence split(
+  const Range1& rng, const Range2& delim) {
     BackInsertionSequence result;
     return std::move(split_into(result, rng, delim));
 }
