7e3b2531b7407b07c53a4c43c8cc1d9a12119bef
Matus Chochlik <chochlik@gmail.com>
2019-06-19 20:26:15 +0200

    Development of the texgen tool

---
+++ source/tools/texgen/parser.cpp
@@ -0,0 +1,27 @@
+/**
+ *  @file texgen/parser.cpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#include "parser.hpp"
+
+namespace oglplus {
+namespace texgen {
+//------------------------------------------------------------------------------
+void parser::begin() {
+    _actions.begin();
+}
+//------------------------------------------------------------------------------
+void parser::parse(input_stream) {
+}
+//------------------------------------------------------------------------------
+void parser::finish() {
+    _actions.finish();
+}
+//------------------------------------------------------------------------------
+} // namespace texgen
+} // namespace oglplus
