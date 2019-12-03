003601c925a9983413fd3268e28a234389f572e6
Matus Chochlik <chochlik@gmail.com>
2019-12-03 20:07:09 +0100

    Added a new example

---
+++ example/eagine/interleaved_call.cpp
@@ -0,0 +1,47 @@
+/**
+ *  @example eagine/interleaved_call.cpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#include <eagine/interleaved_call.hpp>
+#include <eagine/string_span.hpp>
+#include <iostream>
+
+int main() {
+    using namespace eagine;
+    using std::cout;
+    using v = string_view;
+
+    auto fn1 =
+      make_interleaved([](int i) { cout << i; }, []() { cout << ", "; });
+
+    for(int i = 0; i < 20; ++i) {
+        fn1(i);
+    }
+    cout << std::endl;
+
+    auto fn2 = make_interleaved(
+      [](string_view s) { cout << s; }, []() { cout << "}{"; });
+
+    cout << '{';
+    for(auto name : view({v("January"),
+                          v("February"),
+                          v("March"),
+                          v("April"),
+                          v("May"),
+                          v("June"),
+                          v("July"),
+                          v("August"),
+                          v("September"),
+                          v("October"),
+                          v("November"),
+                          v("December")})) {
+        fn2(name);
+    }
+    cout << '}' << std::endl;
+
+    return 0;
+}
