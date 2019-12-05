8e8b7a42eb75e7d63df0412f2b2da3f3494a5be1
Matus Chochlik <chochlik@gmail.com>
2019-12-05 20:41:42 +0100

    Added the first version of the C-API wrapper utils

---
+++ include/eagine/c_api_wrap.hpp
@@ -0,0 +1,150 @@
+/**
+ *  @file eagine/c_api_wrap.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#ifndef EAGINE_C_API_WRAP_HPP
+#define EAGINE_C_API_WRAP_HPP
+
+#include "identity.hpp"
+#include "int_constant.hpp"
+#include "string_span.hpp"
+#include <type_traits>
+
+namespace eagine {
+//------------------------------------------------------------------------------
+template <typename ApiTraits, typename Signature>
+using c_api_function_ptr =
+  typename ApiTraits::template function_pointer<Signature>::type;
+//------------------------------------------------------------------------------
+template <typename ApiTraits, typename Signature>
+class unimplemented_c_api_function;
+//------------------------------------------------------------------------------
+template <
+  typename ApiTraits,
+  typename Signature,
+  c_api_function_ptr<ApiTraits, Signature> function>
+class static_c_api_function;
+//------------------------------------------------------------------------------
+template <typename ApiTraits, typename Signature>
+class dynamic_c_api_function;
+//------------------------------------------------------------------------------
+template <typename ApiTraits, bool IsImplemented>
+class c_api_function_base : public bool_constant<IsImplemented> {
+public:
+    constexpr c_api_function_base(string_view name) noexcept
+      : _name{name} {
+    }
+
+    constexpr bool is_implemented() const noexcept {
+        return IsImplemented;
+    }
+
+    constexpr operator bool() const noexcept {
+        return IsImplemented;
+    }
+    constexpr bool operator!() const noexcept {
+        return !IsImplemented;
+    }
+
+    constexpr string_view name() const noexcept {
+        return _name;
+    }
+
+private:
+    const string_view _name{};
+};
+//------------------------------------------------------------------------------
+template <typename ApiTraits, typename RV, typename... Params>
+class unimplemented_c_api_function<ApiTraits, RV(Params...)>
+  : public c_api_function_base<ApiTraits, false> {
+    using base = c_api_function_base<ApiTraits, false>;
+
+public:
+    constexpr unimplemented_c_api_function(string_view name, const ApiTraits&)
+      : base(name) {
+    }
+
+    template <typename... Args>
+    constexpr std::enable_if_t<sizeof...(Params) == sizeof...(Args), RV>
+    operator()(Args&&...) const noexcept {
+        return {};
+    }
+};
+//------------------------------------------------------------------------------
+template <
+  typename ApiTraits,
+  typename RV,
+  typename... Params,
+  c_api_function_ptr<ApiTraits, RV(Params...)> function>
+class static_c_api_function<ApiTraits, RV(Params...), function>
+  : public c_api_function_base<ApiTraits, true> {
+    using base = c_api_function_base<ApiTraits, true>;
+
+public:
+    constexpr static_c_api_function(string_view name, const ApiTraits&)
+      : base(name) {
+    }
+
+    template <typename... Args>
+    constexpr std::enable_if_t<sizeof...(Params) == sizeof...(Args), RV>
+    operator()(Args&&... args) const noexcept {
+        return function(std::forward<Args>(args)...);
+    }
+};
+//------------------------------------------------------------------------------
+template <typename ApiTraits, typename RV, typename... Params>
+class dynamic_c_api_function<ApiTraits, RV(Params...)>
+  : public c_api_function_base<ApiTraits, true> {
+
+    using base = c_api_function_base<ApiTraits, true>;
+    using function_pointer = c_api_function_ptr<ApiTraits, RV(Params...)>;
+
+public:
+    constexpr dynamic_c_api_function(string_view name, ApiTraits& traits)
+      : base(name)
+      , _function{traits.load_function(name, identity<function_pointer>())} {
+    }
+
+    constexpr operator bool() const noexcept {
+        return bool(_function);
+    }
+    constexpr bool operator!() const noexcept {
+        return !_function;
+    }
+
+    template <typename... Args>
+    constexpr std::enable_if_t<sizeof...(Params) == sizeof...(Args), RV>
+    operator()(Args&&... args) const noexcept {
+        if(_function) {
+            return _function(std::forward<Args>(args)...);
+        }
+        return {};
+    }
+
+private:
+    function_pointer _function{nullptr};
+};
+//------------------------------------------------------------------------------
+template <
+  typename ApiTraits,
+  typename Signature,
+  c_api_function_ptr<ApiTraits, Signature> function,
+  bool IsAvailable>
+using opt_c_api_function = std::conditional_t<
+  IsAvailable,
+  std::conditional_t<
+    bool(function),
+    static_c_api_function<ApiTraits, Signature, function>,
+    dynamic_c_api_function<ApiTraits, Signature>>,
+  unimplemented_c_api_function<ApiTraits, Signature>>;
+//------------------------------------------------------------------------------
+
+} // namespace eagine
+
+#endif // EAGINE_C_API_WRAP_HPP
+
