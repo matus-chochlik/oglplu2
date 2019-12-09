f6426728651fd58e56520a77059f90889d7d8e64
Matus Chochlik <chochlik@gmail.com>
2019-12-09 18:22:30 +0100

    Added valgrind interop functions + added new example

---
+++ include/eagine/interop/valgrind.hpp
@@ -0,0 +1,34 @@
+/**
+ *  @file eagine/interop/valgrind.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#ifndef EAGINE_INTEROP_VALGRIND_HPP
+#define EAGINE_INTEROP_VALGRIND_HPP
+
+#include "../tribool.hpp"
+
+#if defined(__GNUC__) || defined(__clang__)
+// clang-format off
+#if __has_include(<valgrind/valgrind.h>)
+// clang-format on
+#include <valgrind/valgrind.h>
+#endif
+#endif
+
+namespace eagine {
+
+static inline tribool running_on_valgrind() noexcept {
+#if defined(RUNNING_ON_VALGRIND)
+    return bool(RUNNING_ON_VALGRIND);
+#else
+    return indeterminate;
+#endif
+}
+} // namespace eagine
+
+#endif // EAGINE_INTEROP_VALGRIND_HPP
+
