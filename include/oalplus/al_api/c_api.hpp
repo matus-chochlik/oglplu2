99dcc782aa594bbb2a90a9392fb1279065203fe7
Matus Chochlik <chochlik@gmail.com>
2019-12-13 18:28:23 +0100

    Added function wrappers to AL C-API

--- include/oalplus/al_api/c_api.hpp
+++ include/oalplus/al_api/c_api.hpp
@@ -36,6 +36,7 @@ struct basic_al_c_api {
     using float_type = ALfloat;
     using double_type = ALdouble;
     using void_ptr_type = void*;
+    using const_void_ptr_type = const void*;
 
     eagine::opt_c_api_function<
       api_traits,
@@ -168,6 +169,70 @@ struct basic_al_c_api {
     eagine::opt_c_api_function<
       api_traits,
       nothing_t,
+      void(enum_type, int_type),
+      OALPLUS_AL_STATIC_FUNC(Listeneri),
+      has_api>
+      Listeneri;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(enum_type, int_type, int_type, int_type),
+      OALPLUS_AL_STATIC_FUNC(Listener3i),
+      has_api>
+      Listener3i;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(enum_type, const int_type*),
+      OALPLUS_AL_STATIC_FUNC(Listeneriv),
+      has_api>
+      Listeneriv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(enum_type, int_type*),
+      OALPLUS_AL_STATIC_FUNC(GetListeneriv),
+      has_api>
+      GetListeneriv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(enum_type, float_type),
+      OALPLUS_AL_STATIC_FUNC(Listenerf),
+      has_api>
+      Listenerf;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(enum_type, float_type, float_type, float_type),
+      OALPLUS_AL_STATIC_FUNC(Listener3f),
+      has_api>
+      Listener3f;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(enum_type, const float_type*),
+      OALPLUS_AL_STATIC_FUNC(Listenerfv),
+      has_api>
+      Listenerfv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(enum_type, float_type*),
+      OALPLUS_AL_STATIC_FUNC(GetListenerfv),
+      has_api>
+      GetListenerfv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
       void(size_type, name_type*),
       OALPLUS_AL_STATIC_FUNC(GenBuffers),
       has_api>
@@ -188,6 +253,78 @@ struct basic_al_c_api {
       OALPLUS_AL_STATIC_FUNC(IsBuffer),
       has_api>
       IsBuffer;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, const_void_ptr_type, size_type, size_type),
+      OALPLUS_AL_STATIC_FUNC(BufferData),
+      has_api>
+      BufferData;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, int_type),
+      OALPLUS_AL_STATIC_FUNC(Bufferi),
+      has_api>
+      Bufferi;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, int_type, int_type, int_type),
+      OALPLUS_AL_STATIC_FUNC(Buffer3i),
+      has_api>
+      Buffer3i;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, const int_type*),
+      OALPLUS_AL_STATIC_FUNC(Bufferiv),
+      has_api>
+      Bufferiv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, int_type*),
+      OALPLUS_AL_STATIC_FUNC(GetBufferiv),
+      has_api>
+      GetBufferiv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, float_type),
+      OALPLUS_AL_STATIC_FUNC(Bufferf),
+      has_api>
+      Bufferf;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, float_type, float_type, float_type),
+      OALPLUS_AL_STATIC_FUNC(Buffer3f),
+      has_api>
+      Buffer3f;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, const float_type*),
+      OALPLUS_AL_STATIC_FUNC(Bufferfv),
+      has_api>
+      Bufferfv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, float_type*),
+      OALPLUS_AL_STATIC_FUNC(GetBufferfv),
+      has_api>
+      GetBufferfv;
 
     eagine::opt_c_api_function<
       api_traits,
@@ -213,6 +350,70 @@ struct basic_al_c_api {
       has_api>
       IsSource;
 
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, int_type),
+      OALPLUS_AL_STATIC_FUNC(Sourcei),
+      has_api>
+      Sourcei;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, int_type, int_type, int_type),
+      OALPLUS_AL_STATIC_FUNC(Source3i),
+      has_api>
+      Source3i;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, const int_type*),
+      OALPLUS_AL_STATIC_FUNC(Sourceiv),
+      has_api>
+      Sourceiv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, int_type*),
+      OALPLUS_AL_STATIC_FUNC(GetSourceiv),
+      has_api>
+      GetSourceiv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, float_type),
+      OALPLUS_AL_STATIC_FUNC(Sourcef),
+      has_api>
+      Sourcef;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, float_type, float_type, float_type),
+      OALPLUS_AL_STATIC_FUNC(Source3f),
+      has_api>
+      Source3f;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, const float_type*),
+      OALPLUS_AL_STATIC_FUNC(Sourcefv),
+      has_api>
+      Sourcefv;
+
+    eagine::opt_c_api_function<
+      api_traits,
+      nothing_t,
+      void(name_type, enum_type, float_type*),
+      OALPLUS_AL_STATIC_FUNC(GetSourcefv),
+      has_api>
+      GetSourcefv;
+
     constexpr basic_al_c_api(api_traits& traits)
       : GetError("GetError", traits, *this)
       , Enable("Enable", traits, *this)
@@ -230,12 +431,37 @@ struct basic_al_c_api {
       , DistanceModel("DistanceModel", traits, *this)
       , DopplerFactor("DopplerFactor", traits, *this)
       , SpeedOfSound("SpeedOfSound", traits, *this)
+      , Listeneri("Listeneri", traits, *this)
+      , Listener3i("Listener3i", traits, *this)
+      , Listeneriv("Listeneriv", traits, *this)
+      , GetListeneriv("GetListeneriv", traits, *this)
+      , Listenerf("Listenerf", traits, *this)
+      , Listener3f("Listener3f", traits, *this)
+      , Listenerfv("Listenerfv", traits, *this)
+      , GetListenerfv("GetListenerfv", traits, *this)
       , GenBuffers("GenBuffers", traits, *this)
       , DeleteBuffers("DeleteBuffers", traits, *this)
       , IsBuffer("IsBuffer", traits, *this)
+      , BufferData("BufferData", traits, *this)
+      , Bufferi("Bufferi", traits, *this)
+      , Buffer3i("Buffer3i", traits, *this)
+      , Bufferiv("Bufferiv", traits, *this)
+      , GetBufferiv("GetBufferiv", traits, *this)
+      , Bufferf("Bufferf", traits, *this)
+      , Buffer3f("Buffer3f", traits, *this)
+      , Bufferfv("Bufferfv", traits, *this)
+      , GetBufferfv("GetBufferfv", traits, *this)
       , GenSources("GenSources", traits, *this)
       , DeleteSources("DeleteSources", traits, *this)
-      , IsSource("IsSource", traits, *this) {
+      , IsSource("IsSource", traits, *this)
+      , Sourcei("Sourcei", traits, *this)
+      , Source3i("Source3i", traits, *this)
+      , Sourceiv("Sourceiv", traits, *this)
+      , GetSourceiv("GetSourceiv", traits, *this)
+      , Sourcef("Sourcef", traits, *this)
+      , Source3f("Source3f", traits, *this)
+      , Sourcefv("Sourcefv", traits, *this)
+      , GetSourcefv("GetSourcefv", traits, *this) {
     }
 };
 //------------------------------------------------------------------------------
