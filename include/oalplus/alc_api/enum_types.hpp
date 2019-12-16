53e56617661643d6e259a3f2485a085a7acb80d3
Matus Chochlik <chochlik@gmail.com>
2019-12-16 20:09:36 +0100

    Added ALC enumeration types.

---
+++ include/oalplus/alc_api/enum_types.hpp
@@ -0,0 +1,36 @@
+/**
+ *  @file oalplus/alc_api/enum_types.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#ifndef OALPLUS_ALC_API_ENUM_TYPES_HPP
+#define OALPLUS_ALC_API_ENUM_TYPES_HPP
+
+#include "enum_class.hpp"
+
+namespace eagine {
+namespace oalp {
+//------------------------------------------------------------------------------
+struct alc_error_code
+  : alc_enum_class<alc_error_code, api_types::enum_type, 0> {
+    using enum_class::enum_class;
+};
+
+struct context_attrib
+  : alc_enum_class<context_attrib, api_types::enum_type, 1> {
+    using enum_class::enum_class;
+};
+
+struct alc_string_query
+  : alc_enum_class<alc_string_query, api_types::enum_type, 2> {
+    using enum_class::enum_class;
+};
+//------------------------------------------------------------------------------
+} // namespace oalp
+} // namespace eagine
+
+#endif // OALPLUS_ALC_API_ENUM_TYPES_HPP
+
