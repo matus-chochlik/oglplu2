a372b02ce2a90da73f51fda1a4a93ef12f30aa1f
Matus Chochlik <chochlik@gmail.com>
2019-12-28 00:10:41 +0100

    Added memory buffer pool

---
+++ include/eagine/memory/buffer_pool.hpp
@@ -0,0 +1,50 @@
+/**
+ *  @file eagine/memory/buffer_pool.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#ifndef EAGINE_MEMORY_BUFFER_POOL_HPP
+#define EAGINE_MEMORY_BUFFER_POOL_HPP
+
+#include "buffer.hpp"
+#include <algorithm>
+#include <vector>
+
+namespace eagine {
+namespace memory {
+//------------------------------------------------------------------------------
+class buffer_pool {
+public:
+    memory::buffer get(span_size_t req_size = 0) {
+        auto pos = std::lower_bound(
+          _pool.begin(), _pool.end(), req_size, [](auto& buf, auto req) {
+              return buf.size() < req;
+          });
+        if(pos != _pool.end()) {
+            memory::buffer result{std::move(*pos)};
+            _pool.erase(pos);
+            return result;
+        }
+        return {};
+    }
+
+    void eat(memory::buffer used) {
+        auto pos = std::lower_bound(
+          _pool.begin(), _pool.end(), used.size(), [](auto& buf, auto size) {
+              return buf.size() < size;
+          });
+        _pool.emplace(pos, std::move(used));
+    }
+
+private:
+    std::vector<memory::buffer> _pool;
+};
+//------------------------------------------------------------------------------
+} // namespace memory
+} // namespace eagine
+
+#endif // EAGINE_MEMORY_BUFFER_POOL_HPP
