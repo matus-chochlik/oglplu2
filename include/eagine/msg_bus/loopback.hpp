25af24dd798ad97fa4e4555d6a4dcc6b4cc0314b
Matus Chochlik <chochlik@gmail.com>
2019-12-28 15:35:00 +0100

    Added loopback message bus connection implementation

---
+++ include/eagine/msg_bus/loopback.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file eagine/msg_bus/loopback.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#ifndef EAGINE_MSG_BUS_LOOPBACK_HPP
+#define EAGINE_MSG_BUS_LOOPBACK_HPP
+
+#include "connection.hpp"
+#include <algorithm>
+#include <limits>
+#include <mutex>
+#include <vector>
+
+namespace eagine {
+//------------------------------------------------------------------------------
+class message_bus_loopback_connection : public message_bus_connection {
+public:
+    void update() final {
+    }
+
+    bool is_usable() final {
+        return true;
+    }
+
+    span_size_t max_data_size() final {
+        return std::numeric_limits<span_size_t>::max();
+    }
+
+    void send(
+      identifier_t class_id,
+      identifier_t method_id,
+      const message_view& message) {
+        std::unique_lock lock{_mutex};
+        _messages.push(class_id, method_id, message);
+    }
+
+    void fetch_messages(
+      message_bus_connection::fetch_handler handler) noexcept {
+        std::unique_lock lock{_mutex};
+        _messages.fetch_all(handler);
+    }
+
+private:
+    std::mutex _mutex;
+    message_storage _messages;
+};
+//------------------------------------------------------------------------------
+} // namespace eagine
+
+#endif // EAGINE_MSG_BUS_LOOPBACK_HPP
+
