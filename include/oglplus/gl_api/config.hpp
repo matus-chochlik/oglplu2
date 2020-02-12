0db39f09372e94d29175f3e75c08f463c2ff7136
Matus Chochlik <chochlik@gmail.com>
2020-02-12 14:51:57 +0100

    Initial work on the GL API wrapper

---
+++ include/oglplus/gl_api/config.hpp
@@ -0,0 +1,75 @@
+/**
+ *  @file oglplus/gl_api/config.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#ifndef OGLPLUS_GL_API_CONFIG_HPP
+#define OGLPLUS_GL_API_CONFIG_HPP
+
+#include "../config/basic.hpp"
+
+#ifndef OGLPLUS_HAS_GL
+#if defined(GL_SUCCESS)
+#define OGLPLUS_HAS_GL 1
+#else
+#define OGLPLUS_HAS_GL 0
+#endif
+#endif // OGLPLUS_HAS_GL
+
+#if !OGLPLUS_HAS_GL
+#include <eagine/nothing.hpp>
+#include <cstdint>
+#endif
+
+namespace eagine {
+namespace oglp {
+//------------------------------------------------------------------------------
+struct gl_types {
+    using void_ptr_type = void*;
+#if OGLPLUS_HAS_GL
+    static constexpr bool has_api = true;
+    using char_type = GLchar;
+    using bool_type = GLboolean;
+    using enum_type = GLenum;
+    using bitfield_type = GLbitfield;
+    using byte_type = GLbyte;
+    using ubyte_type = GLubyte;
+    using short_type = GLshort;
+    using ushort_type = GLushort;
+    using int_type = GLint;
+    using uint_type = GLuint;
+    using int64_type = GLint64;
+    using uint64_type = GLuint64;
+    using fixed_type = GLfixed;
+    using float_type = GLfloat;
+    using double_type = GLdouble;
+    using sizei_type = GLsizei;
+#else
+    static constexpr bool has_api = false;
+    using char_type = char;
+    using bool_type = bool;
+    using enum_type = unsigned;
+    using bitfield_type = unsigned;
+    using byte_type = std::int8_t;
+    using ubyte_type = std::uint8_t;
+    using short_type = std::int16_t;
+    using ushort_type = std::uint16_t;
+    using int_type = int;
+    using uint_type = unsigned;
+    using int64_type = std::int64_t;
+    using uint64_type = std::uint64_t;
+    using fixed_type = std::int32_t;
+    using float_type = float;
+    using double_type = double;
+    using sizei_type = std::ptrdiff_t;
+#endif
+};
+//------------------------------------------------------------------------------
+} // namespace oglp
+} // namespace eagine
+
+#endif // OGLPLUS_GL_API_CONFIG_HPP
+
