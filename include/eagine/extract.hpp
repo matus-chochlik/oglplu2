163911608641db5fe8411c224d02e7c9d2e2fbe8
Matus Chochlik <chochlik@gmail.com>
2020-02-16 09:04:28 +0100

    Added the ok<Outcome> template

--- include/eagine/extract.hpp
+++ include/eagine/extract.hpp
@@ -67,6 +67,81 @@ extract_or(std::unique_ptr<T, D>& ptr, F&& fallback) {
     return bool(ptr) ? *ptr : T{std::forward<F>(fallback)};
 }
 //------------------------------------------------------------------------------
+template <typename Outcome>
+struct ok_traits {
+    static constexpr nothing_t nok_info(const Outcome&) noexcept {
+        return {};
+    }
+};
+//------------------------------------------------------------------------------
+template <typename Outcome>
+class ok {
+private:
+    Outcome _outcome{};
+
+    using _traits = ok_traits<Outcome>;
+
+public:
+    constexpr inline ok(Outcome&& outcome) noexcept(
+      noexcept(std::declval<Outcome&&>()))
+      : _outcome{std::move(outcome)} {
+    }
+
+    explicit constexpr operator bool() noexcept(
+      noexcept(bool(std::declval<Outcome&>()))) {
+        return bool(_outcome);
+    }
+
+    explicit constexpr operator bool() const
+      noexcept(noexcept(bool(std::declval<const Outcome&>()))) {
+        return bool(_outcome);
+    }
+
+    constexpr decltype(extract(std::declval<Outcome&>())) get() noexcept(
+      noexcept(extract(std::declval<Outcome&>()))) {
+        return extract(_outcome);
+    }
+
+    constexpr decltype(extract(std::declval<const Outcome&>())) get() const
+      noexcept(noexcept(extract(std::declval<const Outcome&>()))) {
+        return extract(_outcome);
+    }
+
+    constexpr operator decltype(extract(std::declval<Outcome&>()))() noexcept(
+      noexcept(extract(std::declval<Outcome&>()))) {
+        return extract(_outcome);
+    }
+
+    constexpr operator decltype(extract(std::declval<const Outcome&>()))() const
+      noexcept(noexcept(extract(std::declval<const Outcome&>()))) {
+        return extract(_outcome);
+    }
+
+    constexpr decltype(_traits::nok_info(std::declval<const Outcome&>())) nok()
+      const noexcept {
+        return _traits::nok_info(_outcome);
+    }
+
+    constexpr decltype(_traits::nok_info(std::declval<const Outcome&>()))
+    operator!() const noexcept {
+        return _traits::nok_info(_outcome);
+    }
+};
+//------------------------------------------------------------------------------
+template <typename Outcome>
+auto begin(
+  const ok<Outcome>& x,
+  decltype(std::declval<const ok<Outcome>&>().get().begin())* = nullptr) {
+    return x.get().begin();
+}
+//------------------------------------------------------------------------------
+template <typename Outcome>
+auto end(
+  const ok<Outcome>& x,
+  decltype(std::declval<const ok<Outcome>&>().get().end())* = nullptr) {
+    return x.get().end();
+}
+//------------------------------------------------------------------------------
 } // namespace eagine
 
 #endif // EAGINE_EXTRACT_HPP
