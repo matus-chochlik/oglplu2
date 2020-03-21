c0f29effa184237a2f63e7c549b787e44d2bf7d0
Matus Chochlik <chochlik@gmail.com>
2020-03-21 17:17:40 +0100

    Added a new GL example

---
+++ example/combined/003_checker_compat.cpp
@@ -0,0 +1,125 @@
+/**
+ *  example oglplus/003_checker_compat.cpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+// clang-format off
+#include <oglplus/gl.hpp>
+#include <oglplus/gl_api.hpp>
+#include <oglplus/glsl/string_ref.hpp>
+
+#include "example.hpp"
+// clang-format on
+#include <iostream>
+
+namespace eagine {
+namespace oglp {
+//------------------------------------------------------------------------------
+class example_spectrum : public example {
+
+    owned_shader_name fs;
+
+    owned_program_name prog;
+
+public:
+    example_spectrum(const example_context& ctx);
+
+    void resize(const example_context& ctx) final;
+
+    void render(const example_context& ctx) final;
+};
+//------------------------------------------------------------------------------
+example_spectrum::example_spectrum(const example_context& ctx) {
+    const auto& [gl, GL] = ctx.gl();
+
+    gl.clear_color(0.4f, 0.4f, 0.4f, 0.0f);
+
+    // fragment shader
+    gl.create_shader(GL.fragment_shader) >> fs;
+    gl.shader_source(
+      fs,
+      glsl_literal(
+        "#version 140\n"
+        "out vec3 fragColor;\n"
+        "void main()\n"
+        "{\n"
+        "	vec2 co = gl_TexCoord[0].xy * vec2(8.0);\n"
+        "	float ck = mod(mod(int(co.x), 2) + mod(int(co.y), 2), 2);\n"
+        "	fragColor = mix(vec3(0.0), gl_Color.rgb, ck);\n"
+        "}\n"));
+    gl.compile_shader(fs);
+
+    std::array<char, 1024> x;
+    std::cout << extract(gl.get_shader_info_log(fs, cover(x))) << std::endl;
+
+    // program
+    gl.create_program() >> prog;
+    gl.attach_shader(prog, fs);
+    gl.link_program(prog);
+    gl.use_program(prog);
+}
+//------------------------------------------------------------------------------
+void example_spectrum::resize(const example_context& ctx) {
+    const auto& state = ctx.state();
+    const auto& [gl, GL] = ctx.gl();
+
+    gl.viewport(state.width(), state.height());
+
+    const auto h = GL.double_(2);
+    const auto w = h * GL.double_(state.aspect());
+
+    gl.matrix_mode(GL.projection);
+    gl.load_identity();
+    gl.ortho(-w, +w, -h, +h, 0, 1);
+}
+//------------------------------------------------------------------------------
+void example_spectrum::render(const example_context& ctx) {
+    const auto& [gl, GL] = ctx.gl();
+    gl.clear(GL.color_buffer_bit);
+
+    gl.matrix_mode(GL.modelview);
+    gl.load_identity();
+    gl.rotate_f(degrees_(ctx.state().exec_time().value() * 90), 0, 0, -1);
+
+    gl.begin(GL.quads);
+
+    gl.tex_coord2i(0, 0);
+    gl.color3f(1.0f, 1.0f, 1.0f);
+    gl.vertex2i(-1, -1);
+
+    gl.tex_coord2i(1, 0);
+    gl.color3f(1.0f, 0.2f, 0.2f);
+    gl.vertex2i(+1, -1);
+
+    gl.tex_coord2i(1, 1);
+    gl.color3f(0.2f, 1.0f, 0.2f);
+    gl.vertex2i(+1, +1);
+
+    gl.tex_coord2i(0, 1);
+    gl.color3f(0.2f, 0.2f, 1.0f);
+    gl.vertex2i(-1, +1);
+
+    gl.end();
+}
+//------------------------------------------------------------------------------
+std::unique_ptr<example> make_example(
+  const example_args&, const example_context& ctx) {
+    return std::make_unique<example_spectrum>(ctx);
+}
+//------------------------------------------------------------------------------
+void adjust_params(example_params& params) {
+    params.compatibility_context(true);
+    params.depth_buffer(false);
+    params.stencil_buffer(false);
+}
+//------------------------------------------------------------------------------
+bool is_example_param(const example_arg&) {
+    return false;
+}
+//------------------------------------------------------------------------------
+} // namespace oglp
+} // namespace eagine
