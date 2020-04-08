8672c1462b53ad7ed95eaa398bad2f7086220929
Matus Chochlik <chochlik@gmail.com>
2020-04-08 19:19:01 +0200

    Added first version of logging backend interface.

---
+++ include/eagine/logging/backend.hpp
@@ -0,0 +1,44 @@
+/**
+ *  @file eagine/logging/backend.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#ifndef EAGINE_LOGGING_BACKEND_HPP
+#define EAGINE_LOGGING_BACKEND_HPP
+
+#include "../identifier.hpp"
+#include "../string_span.hpp"
+#include "severity.hpp"
+
+namespace eagine {
+//------------------------------------------------------------------------------
+struct logger_backend {
+    logger_backend() noexcept = default;
+    logger_backend(logger_backend&&) noexcept = default;
+    logger_backend(const logger_backend&) noexcept = default;
+    logger_backend& operator=(logger_backend&&) noexcept = default;
+    logger_backend& operator=(const logger_backend&) noexcept = default;
+    virtual ~logger_backend() noexcept = default;
+
+    virtual logger_backend* entry_backend(
+      identifier source, log_event_severity severity) noexcept = 0;
+
+    virtual bool begin_message(
+      identifier source,
+      log_event_severity severity,
+      string_view format) noexcept = 0;
+
+    virtual void add_identifier(identifier name, identifier value) noexcept = 0;
+
+    virtual void add_string(identifier name, string_view value) noexcept = 0;
+
+    virtual void finish_message() noexcept = 0;
+};
+//------------------------------------------------------------------------------
+} // namespace eagine
+
+#endif // EAGINE_LOGGING_BACKEND_HPP
