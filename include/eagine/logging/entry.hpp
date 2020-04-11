3c48543eb1d468d8b56957b891d5518e98c30e59
Matus Chochlik <chochlik@gmail.com>
2020-04-11 09:08:34 +0200

    Refactoring in log entry

--- include/eagine/logging/entry.hpp
+++ include/eagine/logging/entry.hpp
@@ -17,6 +17,42 @@
 
 namespace eagine {
 //------------------------------------------------------------------------------
+template <typename T>
+struct log_entry_arg_adapter;
+//------------------------------------------------------------------------------
+template <typename T>
+struct does_have_log_entry_adapter {
+private:
+    template <typename X, std::size_t = sizeof(log_entry_arg_adapter<X>)>
+    static std::true_type _test(X*);
+
+    static std::false_type _test(...);
+
+public:
+    using type = decltype(_test(static_cast<std::decay_t<T>*>(nullptr)));
+};
+
+template <typename T>
+using has_log_entry_adapter_t = typename does_have_log_entry_adapter<T>::type;
+
+template <typename T>
+constexpr const bool has_log_entry_adapter_v =
+  has_log_entry_adapter_t<T>::value;
+//------------------------------------------------------------------------------
+template <typename T>
+static constexpr inline auto adapt_log_entry_arg(identifier name, T&& value) {
+    return log_entry_arg_adapter<std::decay_t<T>>::apply(
+      name, std::forward<T>(value));
+}
+//------------------------------------------------------------------------------
+template <>
+struct log_entry_arg_adapter<identifier> {
+    static constexpr auto apply(identifier name, identifier value) noexcept {
+        return
+          [=](logger_backend& backend) { backend.add_identifier(name, value); };
+    }
+};
+//------------------------------------------------------------------------------
 class logger;
 
 class log_entry {
@@ -42,20 +78,20 @@ public:
         }
     }
 
-    log_entry& arg(identifier name, identifier value) noexcept {
-        if(_backend) {
-            _args.add([=](logger_backend& backend) {
-                backend.add_identifier(name, value);
-            });
-        }
-        return *this;
-    }
-
     log_entry& arg(identifier name, string_view value) noexcept {
         if(_backend) {
             _args.add([=](logger_backend& backend) {
                 backend.add_string(name, value);
             });
+        }
+        return *this;
+    }
+
+    template <typename T>
+    std::enable_if_t<has_log_entry_adapter_v<std::decay_t<T>>, log_entry&> arg(
+      identifier name, T&& value) noexcept {
+        if(_backend) {
+            _args.add(adapt_log_entry_arg(name, std::forward<T>(value)));
         }
         return *this;
     }
