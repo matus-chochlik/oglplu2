d71f3a84af1c8ba41e65b8caeb681a5be167d3d0
Matus Chochlik <chochlik@gmail.com>
2020-04-15 20:29:13 +0200

    Added logging to example

--- example/eagine/message_bus/002_direct.cpp
+++ example/eagine/message_bus/002_direct.cpp
@@ -6,6 +6,7 @@
  *  See accompanying file LICENSE_1_0.txt or copy at
  *   http://www.boost.org/LICENSE_1_0.txt
  */
+#include <eagine/logging/root_logger.hpp>
 #include <eagine/memory/span_algo.hpp>
 #include <eagine/message_bus/acceptor.hpp>
 #include <eagine/message_bus/direct.hpp>
@@ -22,16 +23,18 @@ struct str_utils_server : subscriber<2> {
     using base = subscriber<2>;
     using base::bus;
 
-    str_utils_server(endpoint& ep)
+    str_utils_server(logger& parent, endpoint& ep)
       : base(
           ep,
           this,
           EAGINE_MSG_MAP(StrUtilReq, UpperCase, this_class, uppercase),
-          EAGINE_MSG_MAP(StrUtilReq, Reverse, this_class, reverse)) {
+          EAGINE_MSG_MAP(StrUtilReq, Reverse, this_class, reverse))
+      , _log{EAGINE_ID(Server), parent} {
     }
 
     bool reverse(stored_message& msg) {
         auto str = as_chars(cover(msg.data));
+        _log.trace("received request: ${content}").arg(EAGINE_ID(content), str);
         memory::reverse(str);
         bus().send(EAGINE_MSG_ID(StrUtilRes, Reverse), as_bytes(str));
         return true;
@@ -43,6 +46,9 @@ struct str_utils_server : subscriber<2> {
         bus().send(EAGINE_MSG_ID(StrUtilRes, UpperCase), as_bytes(str));
         return true;
     }
+
+private:
+    logger _log{};
 };
 //------------------------------------------------------------------------------
 struct str_utils_client : subscriber<2> {
@@ -50,12 +56,13 @@ struct str_utils_client : subscriber<2> {
     using base = subscriber<2>;
     using base::bus;
 
-    str_utils_client(endpoint& ep)
+    str_utils_client(logger& parent, endpoint& ep)
       : base(
           ep,
           this,
           EAGINE_MSG_MAP(StrUtilRes, UpperCase, this_class, print),
-          EAGINE_MSG_MAP(StrUtilRes, Reverse, this_class, print)) {
+          EAGINE_MSG_MAP(StrUtilRes, Reverse, this_class, print))
+      , _log{EAGINE_ID(Client), parent} {
     }
 
     void call_reverse(string_view str) {
@@ -69,7 +76,8 @@ struct str_utils_client : subscriber<2> {
     }
 
     bool print(stored_message& msg) {
-        std::cout << view(msg.data) << std::endl;
+        _log.info("received response: ${content}")
+          .arg(EAGINE_ID(content), as_chars(view(msg.data)));
         --_remaining;
         return true;
     }
@@ -79,16 +87,20 @@ struct str_utils_client : subscriber<2> {
     }
 
 private:
+    logger _log{};
     int _remaining{0};
 };
 //------------------------------------------------------------------------------
 } // namespace msgbus
 } // namespace eagine
 
-int main() {
+int main(int argc, const char** argv) {
     using namespace eagine;
 
-    auto acceptor = std::make_unique<msgbus::direct_acceptor>();
+    program_args args(argc, argv);
+    root_logger log(args);
+
+    auto acceptor = std::make_unique<msgbus::direct_acceptor>(log);
 
     msgbus::endpoint server_endpoint;
     msgbus::endpoint client_endpoint;
@@ -96,11 +108,11 @@ int main() {
     server_endpoint.add_connection(acceptor->make_connection());
     client_endpoint.add_connection(acceptor->make_connection());
 
-    msgbus::router router;
+    msgbus::router router(log);
     router.add_acceptor(std::move(acceptor));
 
-    msgbus::str_utils_server server(server_endpoint);
-    msgbus::str_utils_client client(client_endpoint);
+    msgbus::str_utils_server server(log, server_endpoint);
+    msgbus::str_utils_client client(log, client_endpoint);
 
     client.call_reverse("foo");
     client.call_reverse("bar");
