714050759587673df6e6d088261ffdc5b7a708d8
Matus Chochlik <chochlik@gmail.com>
2020-05-01 08:49:43 +0200

    Embed-related updates in the build system

--- config/Embed.cmake
+++ config/Embed.cmake
@@ -8,29 +8,36 @@ set(
 	"${PROJECT_SOURCE_DIR}/config/tools/embed_generator"
 )
 
-function(eagine_embed_resources TARGET_NAME)
+function(eagine_embed_target_resources TARGET_NAME)
 	get_target_property(
 		TARGET_SOURCES
 		${TARGET_NAME}
 		SOURCES
 	)
-	set(RESOURCE_FILE ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}-res.cpp)
-	set(GEN_OPTIONS -o;${RESOURCE_FILE})
+	set(RESOURCE_FILE ${TARGET_NAME}.resources.cpp)
+	set(GEN_DEPENDS)
+	set(GEN_OPTIONS)
+	list(APPEND GEN_OPTIONS -s)
+	list(APPEND GEN_OPTIONS ${CMAKE_CURRENT_BINARY_DIR})
+	list(APPEND GEN_OPTIONS -o)
+	list(APPEND GEN_OPTIONS ${CMAKE_CURRENT_BINARY_DIR}/${RESOURCE_FILE})
 	foreach(TARGET_SOURCE ${TARGET_SOURCES})
 		list(APPEND GEN_OPTIONS -i)
 		list(APPEND GEN_OPTIONS ${TARGET_SOURCE})
+		list(APPEND GEN_DEPENDS ${TARGET_SOURCE})
 	endforeach()
-	execute_process(
+	add_custom_command(
+		OUTPUT ${RESOURCE_FILE}
 		COMMAND ${EAGINE_EMBED_GENERATOR} ${GEN_OPTIONS}
 		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-		RESULT_VARIABLE GENERATE_ERRORS
+		DEPENDS ${GEN_DEPENDS} ${ARGN}
 	)
-	if(NOT GENERATE_ERRORS)
-		target_sources(
-			${TARGET_NAME}
-			PRIVATE ${RESOURCE_FILE}
-		)
-	else()
-		message(FATAL_ERROR "failed to compile resources for ${TARGET_NAME}")
-	endif()
+	set_source_files_properties(
+		${RESOURCE_FILE}
+		PROPERTIES GENERATED 1
+	)
+	target_sources(
+		${TARGET_NAME}
+		PRIVATE ${RESOURCE_FILE}
+	)
 endfunction()
