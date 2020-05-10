4bf04a40966d97e7847dab0d1963dd7539722f63
Matus Chochlik <chochlik@gmail.com>
2020-05-10 18:49:34 +0200

    Added double buffer.

---
+++ include/eagine/double_buffer.hpp
@@ -0,0 +1,49 @@
+/**
+ *  @file eagine/double_buffer.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#ifndef EAGINE_DOUBLE_BUFFER_HPP
+#define EAGINE_DOUBLE_BUFFER_HPP
+
+#include <array>
+
+namespace eagine {
+
+template <typename T>
+class double_buffer {
+public:
+    double_buffer() = default;
+
+    double_buffer(const T& initial)
+      : _values{{initial, initial}} {
+    }
+
+    void swap() noexcept {
+        _idx = !_idx;
+    }
+
+    T& front() noexcept {
+        return _idx ? _values.back() : _values.front();
+    }
+
+    const T& front() const noexcept {
+        return _idx ? _values.back() : _values.front();
+    }
+
+    const T& back() const noexcept {
+        return _idx ? _values.front() : _values.back();
+    }
+
+private:
+    std::array<T, 2> _values{};
+    bool _idx{false};
+};
+
+} // namespace eagine
+
+#endif // EAGINE_DOUBLE_BUFFER_HPP
