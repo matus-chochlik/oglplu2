ec5d6cb9602763e2b7a12fec805e32a7d5eda942
Matus Chochlik <chochlik@gmail.com>
2020-05-13 21:15:55 +0200

    Added / updated development helper scripts

--- devel/ping_pong_r.sh
+++ devel/ping_pong_r.sh
@@ -5,38 +5,25 @@
 #  http://www.boost.org/LICENSE_1_0.txt
 #
 install_prefix="$(<$(dirname ${0})/../INSTALL_PREFIX)"
+log_args=("--use-asio-log" "--min-log-severity" "info")
 conn_type="--msg-bus-asio-local-stream"
-ping_addr="/tmp/ping"
-pong_addr="/tmp/pong"
 #
 pids=()
 #
 ${install_prefix}/bin/eagine-message_bus-router \
+	"${log_args[@]}" \
 	${conn_type} \
-	--msg-bus-router-address ${ping_addr} \
-	--msg-bus-router-id-base 1000 \
-	& pids+=($!)
-sleep 1
-${install_prefix}/bin/eagine-message_bus-router \
-	${conn_type} \
-	--msg-bus-router-address ${pong_addr} \
-	--msg-bus-router-id-base 2000 \
-	& pids+=($!)
-sleep 1
-${install_prefix}/bin/eagine-local-bridge \
-	${conn_type} \
-	-l --msg-bus-router-address ${ping_addr} \
-	-r --msg-bus-router-address ${pong_addr} \
 	& pids+=($!)
 sleep 1
 ${install_prefix}/share/oglplus/examples/eagine-003_pong \
+	"${log_args[@]}" \
 	${conn_type} \
-	--msg-bus-router-address ${pong_addr} \
 	& pids+=($!)
 sleep 1
 ${install_prefix}/share/oglplus/examples/eagine-003_ping \
+	"${log_args[@]}" \
+	--ping-count 1M \
 	${conn_type} \
-	--msg-bus-router-address ${ping_addr} \
 	& pids+=($!)
 
 for pid in ${pids[@]}
