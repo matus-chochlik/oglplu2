f766a811e6326a9e4c5d4c71a7bc6d6b9a2e6a98
Matus Chochlik <chochlik@gmail.com>
2020-05-23 11:26:29 +0200

    Cleanup and updates in the tests

--- test/boost_test/eagine/helper/object_c.hpp
+++
@@ -1,7 +0,0 @@
-/*
- *  Copyright Matus Chochlik.
- *  Distributed under the Boost Software License, Version 1.0.
- *  See accompanying file LICENSE_1_0.txt or copy at
- *   http://www.boost.org/LICENSE_1_0.txt
- */
-#include "../../../compile_test/eagine/helper/object_c.hpp"
