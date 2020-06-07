d6170765aad205e43dc0ff5dbadbf10fd71b0664
Matus Chochlik <chochlik@gmail.com>
2020-06-07 23:44:37 +0200

    Changes in value tree

--- include/eagine/value_tree/interface.hpp
+++ include/eagine/value_tree/interface.hpp
@@ -12,25 +12,31 @@
 
 #include "../string_path.hpp"
 #include "../string_span.hpp"
+#include <cstdint>
 #include <memory>
 #include <string>
 
 namespace eagine {
 namespace valtree {
 //------------------------------------------------------------------------------
+struct compound_interface;
 struct attribute_interface {
     attribute_interface() noexcept = default;
-    attribute_interface(attribute_interface&&) = delete;
-    attribute_interface(const attribute_interface&) = delete;
-    attribute_interface& operator=(attribute_interface&&) = delete;
-    attribute_interface& operator=(const attribute_interface&) = delete;
+    attribute_interface(attribute_interface&&) noexcept = default;
+    attribute_interface(const attribute_interface&) = default;
+    attribute_interface& operator=(attribute_interface&&) noexcept = default;
+    attribute_interface& operator=(const attribute_interface&) = default;
     virtual ~attribute_interface() noexcept = default;
 
-    virtual string_view name() = 0;
-    virtual span_size_t nested_count() = 0;
-    virtual std::shared_ptr<attribute_interface> nested(span_size_t index) = 0;
-    virtual std::shared_ptr<attribute_interface> find(
-      const basic_string_path&) = 0;
+    virtual string_view name(compound_interface&) = 0;
+
+    virtual span_size_t nested_count(compound_interface&) = 0;
+
+    virtual attribute_interface* nested(
+      compound_interface&, span_size_t index) = 0;
+
+    virtual attribute_interface* find(
+      compound_interface&, const basic_string_path&) = 0;
 };
 //------------------------------------------------------------------------------
 struct compound_interface {
@@ -41,20 +47,27 @@ struct compound_interface {
     compound_interface& operator=(const compound_interface&) = delete;
     virtual ~compound_interface() noexcept = default;
 
-    virtual std::shared_ptr<attribute_interface> structure() = 0;
+    virtual attribute_interface* structure() = 0;
 
-    virtual span_size_t value_count(
-      const std::shared_ptr<attribute_interface>&) = 0;
+    virtual void add_ref(attribute_interface&) noexcept = 0;
+    virtual void release(attribute_interface&) noexcept = 0;
 
-    virtual span_size_t fetch_values(
-      const std::shared_ptr<attribute_interface>&,
-      span_size_t offset,
-      span<float> dest) = 0;
+    virtual span_size_t value_count(attribute_interface&) = 0;
 
     virtual span_size_t fetch_values(
-      const std::shared_ptr<attribute_interface>&,
-      span_size_t offset,
-      span<std::string> dest) = 0;
+      attribute_interface&, span_size_t offset, span<std::int16_t> dest) = 0;
+
+    virtual span_size_t fetch_values(
+      attribute_interface&, span_size_t offset, span<std::int32_t> dest) = 0;
+
+    virtual span_size_t fetch_values(
+      attribute_interface&, span_size_t offset, span<std::int64_t> dest) = 0;
+
+    virtual span_size_t fetch_values(
+      attribute_interface&, span_size_t offset, span<float> dest) = 0;
+
+    virtual span_size_t fetch_values(
+      attribute_interface&, span_size_t offset, span<std::string> dest) = 0;
 };
 //------------------------------------------------------------------------------
 } // namespace valtree
--- include/eagine/value_tree/wrappers.hpp
+++ include/eagine/value_tree/wrappers.hpp
@@ -10,8 +10,10 @@
 #ifndef EAGINE_VALUE_TREE_WRAPPERS_HPP
 #define EAGINE_VALUE_TREE_WRAPPERS_HPP
 
+#include "../assert.hpp"
 #include "../memory/span_algo.hpp"
 #include "interface.hpp"
+#include <utility>
 
 namespace eagine {
 namespace valtree {
@@ -20,39 +22,79 @@ class compound;
 class attribute {
 public:
     attribute() noexcept = default;
-
-    explicit operator bool() const {
-        return bool(_pimpl);
+    attribute(attribute&& temp) noexcept
+      : _owner{std::move(temp._owner)}
+      , _pimpl{temp._pimpl} {
+        temp._pimpl = nullptr;
     }
 
-    bool operator!() const {
-        return !_pimpl;
+    attribute(const attribute& that)
+      : _owner{that._owner}
+      , _pimpl{that._pimpl} {
+        if(_owner && _pimpl) {
+            _owner->add_ref(*_pimpl);
+        }
+    }
+
+    attribute& operator=(attribute&& temp) noexcept {
+        if(this != &temp) {
+            using std::swap;
+            swap(_owner, temp._owner);
+            swap(_pimpl, temp._pimpl);
+        }
+        return *this;
+    }
+
+    attribute& operator=(const attribute& that) {
+        if(this != &that) {
+            using std::swap;
+            attribute temp{};
+            swap(_owner, temp._owner);
+            swap(_pimpl, temp._pimpl);
+            _owner = that._owner;
+            _pimpl = that._pimpl;
+            if(_owner && _pimpl) {
+                _owner->add_ref(*_pimpl);
+            }
+        }
+        return *this;
+    }
+
+    ~attribute() noexcept {
+        if(_pimpl) {
+            EAGINE_ASSERT(_owner);
+            _owner->release(*_pimpl);
+        }
+    }
+
+    explicit operator bool() const {
+        return _owner && _pimpl;
     }
 
     string_view name() const {
-        if(_pimpl) {
-            return _pimpl->name();
+        if(_owner && _pimpl) {
+            return _pimpl->name(*_owner);
         }
         return {};
     }
 
     span_size_t nested_count() const {
-        if(_pimpl) {
-            return _pimpl->nested_count();
+        if(_owner && _pimpl) {
+            return _pimpl->nested_count(*_owner);
         }
         return 0;
     }
 
     attribute nested(span_size_t index) const {
-        if(_pimpl) {
-            return {_pimpl->nested(index)};
+        if(_owner && _pimpl) {
+            return {_owner, _pimpl->nested(*_owner, index)};
         }
         return {};
     }
 
     attribute find(const basic_string_path& path) const {
-        if(_pimpl) {
-            return {_pimpl->find(path)};
+        if(_owner && _pimpl) {
+            return {_owner, _pimpl->find(*_owner, path)};
         }
         return {};
     }
@@ -64,11 +106,15 @@ public:
 private:
     friend class compound;
 
-    attribute(std::shared_ptr<attribute_interface> pimpl) noexcept
-      : _pimpl{std::move(pimpl)} {
+    attribute(
+      std::shared_ptr<compound_interface> owner,
+      attribute_interface* pimpl) noexcept
+      : _owner{std::move(owner)}
+      , _pimpl{pimpl} {
     }
 
-    std::shared_ptr<attribute_interface> _pimpl{};
+    std::shared_ptr<compound_interface> _owner{};
+    attribute_interface* _pimpl{nullptr};
 };
 //------------------------------------------------------------------------------
 class compound {
@@ -92,14 +138,14 @@ public:
 
     attribute structure() {
         if(_pimpl) {
-            return {_pimpl->structure()};
+            return {_pimpl, _pimpl->structure()};
         }
         return {};
     }
 
     span_size_t value_count(const attribute& attrib) {
-        if(_pimpl) {
-            return _pimpl->value_count(attrib._pimpl);
+        if(_pimpl && attrib._pimpl) {
+            return _pimpl->value_count(*attrib._pimpl);
         }
         return 0;
     }
@@ -107,16 +153,27 @@ public:
     template <typename T>
     span<T> fetch_values(
       const attribute& attrib, span_size_t offset, span<T> dest) {
-        if(_pimpl) {
+        if(_pimpl && attrib._pimpl) {
             return head(
-              dest, _pimpl->fetch_values(attrib._pimpl, offset, dest));
+              dest, _pimpl->fetch_values(*attrib._pimpl, offset, dest));
         }
         return {};
     }
 
     template <typename T>
+    span<T> fetch_values(
+      string_view path_str, span_size_t offset, span<T> dest) {
+        return fetch_values(structure().find(path_str), offset, dest);
+    }
+
+    template <typename T>
     span<T> fetch_values(const attribute& attrib, span<T> dest) {
         return fetch_values(attrib, 0, dest);
+    }
+
+    template <typename T>
+    span<T> fetch_values(string_view path_str, span<T> dest) {
+        return fetch_values(path_str, 0, dest);
     }
 
     template <typename T>
