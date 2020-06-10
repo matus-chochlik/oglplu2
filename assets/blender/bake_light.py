363585605d900eeb23f51c054e76af6dc8148bb3
Matus Chochlik <chochlik@gmail.com>
2020-06-10 18:04:51 +0200

    Minor comment fix

--- assets/blender/bake_light.py
+++ assets/blender/bake_light.py
@@ -5,7 +5,7 @@
 # See accompanying file LICENSE_1_0.txt or copy at
 #  http://www.boost.org/LICENSE_1_0.txt
 #
-# usage: blender -b myfile.blend -P render_cubemap.py [-- [--help|options...]]
+# usage: blender -b myfile.blend -P bake_light.py [-- [--help|options...]]
 
 import os
 import sys
