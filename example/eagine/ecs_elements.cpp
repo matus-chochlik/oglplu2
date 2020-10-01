197d6a460b945f81120a722c828e7afe3ddfd396
Matus Chochlik <chochlik@gmail.com>
2020-10-01 21:40:03 +0200

    Additions to the ECS example

--- example/eagine/ecs_elements.cpp
+++ example/eagine/ecs_elements.cpp
@@ -28,6 +28,10 @@ struct element_name : ecs::component<element_name> {
     element_name(std::string lat, std::string eng)
       : latin{std::move(lat)}
       , english{std::move(eng)} {}
+
+    element_name(std::string lat)
+      : latin{std::move(lat)}
+      , english{latin} {}
 };
 //------------------------------------------------------------------------------
 template <bool Const>
@@ -149,7 +153,7 @@ void populate(ecs::basic_manager<std::string>& elements) {
 
     elements.add(
       "He",
-      element_name("Helium", "Helium"),
+      element_name("Helium"),
       element_protons(2),
       element_period(1),
       element_group(18),
@@ -157,7 +161,7 @@ void populate(ecs::basic_manager<std::string>& elements) {
 
     elements.add(
       "Li",
-      element_name("Lithium", "Lithium"),
+      element_name("Lithium"),
       element_protons(3),
       element_period(2),
       element_group(1),
@@ -165,7 +169,7 @@ void populate(ecs::basic_manager<std::string>& elements) {
 
     elements.add(
       "Be",
-      element_name("Beryllium", "Beryllium"),
+      element_name("Beryllium"),
       element_protons(4),
       element_period(2),
       element_group(2),
@@ -213,23 +217,215 @@ void populate(ecs::basic_manager<std::string>& elements) {
 
     elements.add(
       "Ne",
-      element_name("Neon", "Neon"),
+      element_name("Neon"),
       element_protons(10),
       element_period(2),
       element_group(18),
       atomic_weight(20.1797F));
 
     elements.add(
+      "Na",
+      element_name("Natrium", "Sodium"),
+      element_protons(11),
+      element_period(3),
+      element_group(1),
+      atomic_weight(22.98976928F));
+
+    elements.add(
+      "Mg",
+      element_name("Magnesium"),
+      element_protons(12),
+      element_period(3),
+      element_group(2),
+      atomic_weight(24.305F));
+
+    elements.add(
+      "Al",
+      element_name("Aluminum"),
+      element_protons(13),
+      element_period(3),
+      element_group(13),
+      atomic_weight(26.9815384F));
+
+    elements.add(
+      "Si",
+      element_name("Silicium", "Silicon"),
+      element_protons(14),
+      element_period(3),
+      element_group(14),
+      atomic_weight(28.085F));
+
+    elements.add(
+      "P",
+      element_name("Phosphorus"),
+      element_protons(15),
+      element_period(3),
+      element_group(15),
+      atomic_weight(30.973761998F));
+
+    elements.add(
+      "S",
+      element_name("Sulfur"),
+      element_protons(16),
+      element_period(3),
+      element_group(16),
+      atomic_weight(32.06F));
+
+    elements.add(
+      "Cl",
+      element_name("Chlorum", "Chlorine"),
+      element_protons(17),
+      element_period(3),
+      element_group(17),
+      atomic_weight(35.45F));
+
+    elements.add(
       "Ar",
-      element_name("Argon", "Argon"),
+      element_name("Argon"),
       element_protons(18),
       element_period(3),
       element_group(18),
       atomic_weight(39.95F));
 
     elements.add(
+      "K",
+      element_name("Kalium", "Potasium"),
+      element_protons(19),
+      element_period(4),
+      element_group(1),
+      atomic_weight(39.0983F));
+
+    elements.add(
+      "Ca",
+      element_name("Calcium"),
+      element_protons(20),
+      element_period(4),
+      element_group(2),
+      atomic_weight(40.078F));
+
+    elements.add(
+      "Sc",
+      element_name("Scandium"),
+      element_protons(21),
+      element_period(4),
+      element_group(3),
+      atomic_weight(44.955908F));
+
+    elements.add(
+      "Ti",
+      element_name("Titanium"),
+      element_protons(22),
+      element_period(4),
+      element_group(4),
+      atomic_weight(47.867F));
+
+    elements.add(
+      "V",
+      element_name("Vanadium"),
+      element_protons(23),
+      element_period(4),
+      element_group(5),
+      atomic_weight(50.9415F));
+
+    elements.add(
+      "Cr",
+      element_name("Chromium"),
+      element_protons(24),
+      element_period(4),
+      element_group(6),
+      atomic_weight(51.9961F));
+
+    elements.add(
+      "Mn",
+      element_name("Manganum", "Manganese"),
+      element_protons(25),
+      element_period(4),
+      element_group(7),
+      atomic_weight(54.938043F));
+
+    elements.add(
+      "Fe",
+      element_name("Ferrum", "Iron"),
+      element_protons(26),
+      element_period(4),
+      element_group(8),
+      atomic_weight(55.845F));
+
+    elements.add(
+      "Co",
+      element_name("Cobaltum", "Cobalt"),
+      element_protons(27),
+      element_period(4),
+      element_group(9),
+      atomic_weight(58.933194F));
+
+    elements.add(
+      "Ni",
+      element_name("Niccolum", "Nickel"),
+      element_protons(28),
+      element_period(4),
+      element_group(10),
+      atomic_weight(58.6934F));
+
+    elements.add(
+      "Cu",
+      element_name("Cuprum", "Copper"),
+      element_protons(29),
+      element_period(4),
+      element_group(11),
+      atomic_weight(63.546F));
+
+    elements.add(
+      "Zn",
+      element_name("Zincum", "Zinc"),
+      element_protons(30),
+      element_period(4),
+      element_group(12),
+      atomic_weight(65.38F));
+
+    elements.add(
+      "Ga",
+      element_name("Gallium"),
+      element_protons(31),
+      element_period(4),
+      element_group(13),
+      atomic_weight(69.723F));
+
+    elements.add(
+      "Ge",
+      element_name("Germanium"),
+      element_protons(32),
+      element_period(4),
+      element_group(14),
+      atomic_weight(72.630F));
+
+    elements.add(
+      "As",
+      element_name("Arsenicum", "Arsenic"),
+      element_protons(33),
+      element_period(4),
+      element_group(15),
+      atomic_weight(74.921595F));
+
+    elements.add(
+      "Se",
+      element_name("Selenium"),
+      element_protons(34),
+      element_period(4),
+      element_group(16),
+      atomic_weight(78.971F));
+
+    elements.add(
+      "Br",
+      element_name("Bromum", "Bromine"),
+      element_protons(35),
+      element_period(4),
+      element_group(17),
+      atomic_weight(79.904F));
+
+    elements.add(
       "Kr",
-      element_name("Krypton", "Krypton"),
+      element_name("Krypton"),
       element_protons(36),
       element_period(4),
       element_group(18),
@@ -237,7 +433,7 @@ void populate(ecs::basic_manager<std::string>& elements) {
 
     elements.add(
       "Xe",
-      element_name("Xenon", "Xenon"),
+      element_name("Xenon"),
       element_protons(54),
       element_period(5),
       element_group(18),
@@ -253,7 +449,7 @@ void populate(ecs::basic_manager<std::string>& elements) {
 
     elements.add(
       "Rn",
-      element_name("Radon", "Radon"),
+      element_name("Radon"),
       element_protons(86),
       element_period(6),
       element_group(18),
@@ -261,14 +457,14 @@ void populate(ecs::basic_manager<std::string>& elements) {
 
     elements.add(
       "U",
-      element_name("Uranium", "Uranium"),
+      element_name("Uranium"),
       element_protons(92),
       element_period(7),
       atomic_weight(238.02891F));
 
     elements.add(
       "Og",
-      element_name("Oganesson", "Oganesson"),
+      element_name("Oganesson"),
       element_protons(118),
       element_period(7),
       element_group(18),
