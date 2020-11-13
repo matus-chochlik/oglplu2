22d084a3660f1765a35733d05b69a126b3a4f826
Matus Chochlik <chochlik@gmail.com>
2020-11-13 18:00:49 +0100

    Minor changes in the message bus command scripts

--- devel/ping_pong_registry.sh
+++ devel/ping_pong_registry.sh
@@ -8,24 +8,28 @@ install_prefix="$(<$(dirname ${0})/../INSTALL_PREFIX)"
 variant=${1:-007}
 log_args=("--use-asio-log" "--min-log-severity" "stat")
 conn_type="--msg-bus-asio-local-stream"
+ping_addr="/tmp/ping_reg"
 #
 pids=()
 #
 ${install_prefix}/bin/eagine-message_bus-router \
 	"${log_args[@]}" \
 	${conn_type} \
+	--msg-bus-router-address ${ping_addr} \
 	--msg-bus-router-id-base 1000 \
 	& pids+=($!)
 sleep 1
 ${install_prefix}/share/oglplus/examples/eagine-007_ping \
 	"${log_args[@]}" \
 	${conn_type} \
+	--msg-bus-router-address ${ping_addr} \
 	--ping-count ${2:-1M} \
 	& pids+=($!)
 sleep 5
 ${install_prefix}/share/oglplus/examples/eagine-${variant}_pong_registry \
 	"${log_args[@]}" \
 	${conn_type} \
+	--msg-bus-router-address ${ping_addr} \
 	--ponger-count ${3:-4} \
 	--msg-bus-router-id-base 2000 \
 	& pids+=($!)
