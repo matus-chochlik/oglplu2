ba5da3725113bd8e6a2f99c2ba8db6b0f84fca78
Matus Chochlik <chochlik@gmail.com>
2020-11-14 12:45:59 +0100

    Added a new external example

---
+++ example/external/embed_self/main.cpp
@@ -0,0 +1,18 @@
+/**
+ *  @example eagine/embed_self.cpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#include <eagine/embed.hpp>
+#include <eagine/hexdump.hpp>
+#include <iostream>
+
+auto main() -> int {
+    using namespace eagine;
+    std::cout << hexdump(embed(EAGINE_ID(self), __FILE__));
+
+    return 0;
+}
