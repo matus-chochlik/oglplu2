e92fa16112d6d4cee1b7a43aad4b1c47c8383c9c
Matus Chochlik <chochlik@gmail.com>
2020-11-14 14:52:26 +0100

    Multiple changes in the build system
    - Added instructions to search for GZip.
    - Reimplemented the texture baking functions.
    - Moved textures into the assets directory.

---
+++ config/Baking.cmake
@@ -0,0 +1,71 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#   http://www.boost.org/LICENSE_1_0.txt
+#
+
+function(oglplus_do_add_generated_texture GENERATOR TEX_NAME INPUT)
+
+	if(TARGET ${OGLPLUS_TARGET_PREFIX}oglplus-bake_${GENERATOR}_image)
+		configure_file("${INPUT}" "${CMAKE_CURRENT_BINARY_DIR}/${TEX_NAME}.proctex")
+		file(
+			STRINGS "${CMAKE_CURRENT_BINARY_DIR}/${TEX_NAME}.proctex"
+			PARAMETER_STRING
+		)
+	string(REPLACE "\;" ";" RAW_PARAMETERS ${PARAMETER_STRING})
+
+		set(PARAMETERS)
+		foreach(ITEM ${RAW_PARAMETERS})
+			if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${ITEM}")
+				list(APPEND PARAMETERS "${CMAKE_CURRENT_SOURCE_DIR}/${ITEM}")
+			else()
+				list(APPEND PARAMETERS "${ITEM}")
+			endif()
+		endforeach()
+
+		list(APPEND PARAMETERS "--output")
+		list(APPEND PARAMETERS "${CMAKE_CURRENT_BINARY_DIR}/${TEX_NAME}.oglptex")
+
+		add_custom_command(
+			OUTPUT "${TEX_NAME}.oglptex"
+			COMMAND "${OGLPLUS_TARGET_PREFIX}oglplus-bake_${GENERATOR}_image"
+			ARGS ${PARAMETERS}
+			DEPENDS ${INPUT}
+			COMMENT "Baking texture image ${TEX_NAME}"
+		)
+		add_custom_target(
+			${TEX_NAME}
+			DEPENDS "${TEX_NAME}.oglptex"
+		)
+	else()
+		message(
+			WARNING
+			"Missing '${GENERATOR}' generator, for texture image '${TEX_NAME}'"
+		)
+	endif()
+endfunction()
+
+function(oglplus_add_texture TEX_NAME)
+	file(GLOB PROCTEX "${TEX_NAME}.*.proctex")
+	if("${PROCTEX}" MATCHES "^.*${TEX_NAME}\.(.*)\.proctex$")
+		oglplus_do_add_generated_texture(
+			${CMAKE_MATCH_1}
+			${TEX_NAME}
+			${PROCTEX}
+		)
+	else()
+		if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${TEX_NAME}.png")
+			file(
+				WRITE "${CMAKE_CURRENT_BINARY_DIR}/${TEX_NAME}.png.proctex"
+				"--input;${CMAKE_CURRENT_SOURCE_DIR}/${TEX_NAME}.png"
+			)
+			oglplus_do_add_generated_texture(
+				png
+				${TEX_NAME}
+				"${CMAKE_CURRENT_BINARY_DIR}/${TEX_NAME}.png.proctex"
+			)
+		else()
+			message(FATAL_ERROR "Invalid input(s) for texture '${TEX_NAME}'")
+		endif()
+	endif()
+endfunction()
