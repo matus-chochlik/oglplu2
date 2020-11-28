113686545c1b3760bf1a3ba8872011bdbc4eeec6
Matus Chochlik <chochlik@gmail.com>
2020-11-28 04:47:09 +0100

    Added the make_instance_id function

---
+++ include/eagine/process.hpp
@@ -0,0 +1,35 @@
+/**
+ *  @file eagine/process.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+#ifndef EAGINE_PROCESS_HPP
+#define EAGINE_PROCESS_HPP
+
+#include "config/platform.hpp"
+#include "identifier_t.hpp"
+#include "integer_hash.hpp"
+#include <chrono>
+
+#if EAGINE_POSIX
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
+namespace eagine {
+//------------------------------------------------------------------------------
+static inline auto make_instance_id() -> identifier_t {
+    return
+#if EAGINE_POSIX
+      integer_hash<identifier_t>(::getpid()) ^
+#endif
+      integer_hash<identifier_t>(
+        std::chrono::steady_clock::now().time_since_epoch().count());
+}
+//------------------------------------------------------------------------------
+} // namespace eagine
+
+#endif // EAGINE_PROCESS_HPP
