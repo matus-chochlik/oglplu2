0b736e785068c71acc67362f003979b5b3ab879c
Matus Chochlik <chochlik@gmail.com>
2020-11-30 18:16:28 +0100

    Added initial setup for generating of Doxygen docs

---
+++ doc/doxygen/include/eglplus.hpp
@@ -0,0 +1,17 @@
+/**
+ *  @file eglplus.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+/// @mainpage EGLplus
+///
+/// Implements EGL wrapper code and framebuffer setup utilities.
+
+namespace eagine {
+/// @brief EGLplus code is placed inside of this namespace.
+namespace eglp {}
+} // namespace eagine
---
+++ doc/doxygen/include/oglplus.hpp
@@ -0,0 +1,17 @@
+/**
+ *  @file oalplus.hpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+/// @mainpage OALplus
+///
+/// Implements OpenAL wrapper code and audio utilities.
+
+namespace eagine {
+/// @brief OALplus code is placed inside of this namespace.
+namespace oalp {}
+} // namespace eagine
