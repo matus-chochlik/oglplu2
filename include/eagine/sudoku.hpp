1eab58b60fdcac20d99e755ff7d1a2bf7923156a
Matus Chochlik <chochlik@gmail.com>
2020-12-06 08:14:46 +0100

    Renaming and refactoring in the sudoku board implementation

--- include/eagine/sudoku.hpp
+++ include/eagine/sudoku.hpp
@@ -12,65 +12,75 @@
 #include "assert.hpp"
 #include <array>
 #include <cstdint>
+#include <functional>
 #include <ostream>
 #include <random>
 
 namespace eagine {
 //------------------------------------------------------------------------------
 template <unsigned S>
+class basic_sudoku_glyph;
+template <unsigned S, bool Tight = false>
 class basic_sudoku_board;
+template <unsigned S>
+class basic_sudoku_board_generator;
 //------------------------------------------------------------------------------
 template <unsigned S>
-class basic_sudoku_board_type {
+class basic_sudoku_board_traits {
 public:
     static constexpr const unsigned glyph_count = S * S;
-    static constexpr const unsigned cell_count = S * S * S * S;
 
-    using This = basic_sudoku_board_type;
-    using cell_type = std::conditional_t<
-      (glyph_count > 32U),
-      std::int64_t,
-      std::conditional_t<
-        (glyph_count > 16),
-        std::uint32_t,
-        std::conditional_t<(glyph_count > 8), std::uint16_t, std::uint8_t>>>;
+    using This = basic_sudoku_board_traits;
+    using generator = basic_sudoku_board_generator<S>;
+    using board_type = basic_sudoku_board<S>;
 
-    basic_sudoku_board_type(
+    basic_sudoku_board_traits(
       std::array<std::string, glyph_count> glyphs,
-      std::string empty_glyph,
-      std::string multi_glyph) noexcept
+      std::array<std::string, S> multi_glyphs,
+      std::string empty_glyph) noexcept
       : _glyphs{std::move(glyphs)}
-      , _empty_glyph{std::move(empty_glyph)}
-      , _multi_glyph{std::move(multi_glyph)} {}
+      , _multi_glyphs{std::move(multi_glyphs)}
+      , _empty_glyph{std::move(empty_glyph)} {}
 
-    auto print(std::ostream&, const basic_sudoku_board<S>& board) const
+    auto make_diagonal() -> board_type;
+    auto make_generator() -> generator;
+
+    auto print(std::ostream&, const basic_sudoku_board<S, false>& board) const
       -> std::ostream&;
 
-    auto print_tight(std::ostream&, const basic_sudoku_board<S>& board) const
+    auto print(std::ostream&, const basic_sudoku_board<S, true>& board) const
       -> std::ostream&;
 
 private:
     std::array<std::string, glyph_count> _glyphs;
+    std::array<std::string, S> _multi_glyphs;
     std::string _empty_glyph;
-    std::string _multi_glyph;
 };
 //------------------------------------------------------------------------------
 template <unsigned S>
-class basic_sudoku_value {
+class basic_sudoku_glyph {
 public:
-    struct from_cell_value_tag {};
-    using board_type = basic_sudoku_board_type<S>;
-    using value_type = unsigned;
-    using cell_type = typename board_type::cell_type;
-    static constexpr const unsigned glyph_count = board_type::glyph_count;
+    using board_traits = basic_sudoku_board_traits<S>;
+    using cell_type = std::conditional_t<
+      (board_traits::glyph_count > 32U),
+      std::int64_t,
+      std::conditional_t<
+        (board_traits::glyph_count > 16),
+        std::uint32_t,
+        std::conditional_t<
+          (board_traits::glyph_count > 8),
+          std::uint16_t,
+          std::uint8_t>>>;
 
-    static constexpr auto to_cell_type(value_type value) noexcept {
-        return cell_type(1U << value);
+    static constexpr const unsigned glyph_count = board_traits::glyph_count;
+
+    static constexpr auto to_cell_type(unsigned index) noexcept {
+        return cell_type(1U << index);
     }
 
-    constexpr basic_sudoku_value() noexcept = default;
-    constexpr basic_sudoku_value(value_type value) noexcept
-      : _cel_val{to_cell_type(value)} {}
+    constexpr basic_sudoku_glyph() noexcept = default;
+    constexpr basic_sudoku_glyph(unsigned index) noexcept
+      : _cel_val{to_cell_type(index)} {}
 
     constexpr auto is_empty() const noexcept {
         return _cel_val == 0U;
@@ -80,7 +90,7 @@ public:
         return _is_pot(_cel_val);
     }
 
-    constexpr auto is_unsolved() const noexcept {
+    constexpr auto is_multiple() const noexcept {
         return !is_empty() && !is_single();
     }
 
@@ -88,8 +98,8 @@ public:
         return _cel_val;
     }
 
-    auto get() const noexcept -> value_type {
-        value_type result = 0U;
+    auto get_index() const noexcept -> unsigned {
+        unsigned result = 0U;
         while(result < glyph_count) {
             if(_cel_val == to_cell_type(result)) {
                 break;
@@ -100,51 +110,53 @@ public:
         return result;
     }
 
-    constexpr auto set(value_type value) noexcept -> auto& {
-        EAGINE_ASSERT(value < glyph_count);
-        _cel_val = to_cell_type(value);
+    constexpr auto set(unsigned index) noexcept -> auto& {
+        EAGINE_ASSERT(index < glyph_count);
+        _cel_val = to_cell_type(index);
         return *this;
     }
 
-    constexpr auto add(value_type value) noexcept -> auto& {
-        EAGINE_ASSERT(value < glyph_count);
-        _cel_val |= to_cell_type(value);
+    constexpr auto add(unsigned index) noexcept -> auto& {
+        EAGINE_ASSERT(index < glyph_count);
+        _cel_val |= to_cell_type(index);
         return *this;
     }
 
     template <typename Function>
     void for_each_alternative(Function func) const noexcept {
 
-        for(value_type value = 0; value < glyph_count; ++value) {
-            const auto mask = to_cell_type(value);
+        for(unsigned index = 0; index < glyph_count; ++index) {
+            const auto mask = to_cell_type(index);
             if((_cel_val & mask) == mask) {
-                func(value);
+                func(index);
             }
         }
     }
 
 private:
-    friend class basic_sudoku_board<S>;
+    friend class basic_sudoku_board<S, false>;
+    friend class basic_sudoku_board<S, true>;
 
-    constexpr basic_sudoku_value(cell_type cel_val, from_cell_value_tag) noexcept
+    struct from_cell_value_tag {};
+    constexpr basic_sudoku_glyph(cell_type cel_val, from_cell_value_tag) noexcept
       : _cel_val{cel_val} {}
 
     static constexpr auto _is_pot(cell_type v) noexcept {
-        return (v > 0U) && ((v & (v - 1U)) == 0U);
+        return (v != 0U) && ((v & (v - 1U)) == 0U);
     }
 
     cell_type _cel_val{0U};
 };
 //------------------------------------------------------------------------------
-template <unsigned S>
+template <unsigned S, bool Tight>
 class basic_sudoku_board {
 public:
     using This = basic_sudoku_board;
-    using board_type = basic_sudoku_board_type<S>;
-    using value_type = basic_sudoku_value<S>;
-    using cell_type = typename board_type::cell_type;
+    using board_traits = basic_sudoku_board_traits<S>;
+    using glyph_type = basic_sudoku_glyph<S>;
+    using cell_type = typename glyph_type::cell_type;
     using coord_type = std::array<unsigned, 4>;
-    static constexpr const unsigned glyph_count = board_type::glyph_count;
+    static constexpr const unsigned glyph_count = board_traits::glyph_count;
 
     static constexpr auto invalid_coord() noexcept -> coord_type {
         return {{S, S, S, S}};
@@ -165,13 +177,17 @@ public:
         }
     }
 
-    basic_sudoku_board(const board_type& type) noexcept
+    basic_sudoku_board(const board_traits& type) noexcept
       : _type{type} {
         for_each_coord([&](const auto& coord) {
-            set(coord, value_type());
+            set(coord, glyph_type());
             return true;
         });
     }
+
+    basic_sudoku_board(const basic_sudoku_board<S, !Tight>& that) noexcept
+      : _type{that._type}
+      , _blocks{that._blocks} {}
 
     auto type() const noexcept -> auto& {
         return _type;
@@ -182,11 +198,7 @@ public:
         return that._type.get().print(out, that);
     }
 
-    auto print_tight(std::ostream& out) const -> std::ostream& {
-        return _type.get().print_tight(out, *this);
-    }
-
-    auto is_possible(const coord_type& coord, value_type value) const noexcept {
+    auto is_possible(const coord_type& coord, glyph_type value) const noexcept {
         const auto [cbx, cby, ccx, ccy] = coord;
         const auto cel_val = value.cell_value();
 
@@ -229,8 +241,6 @@ public:
             if(!value.is_single()) {
                 result = false;
                 return false;
-            } else {
-                EAGINE_ASSERT(is_possible(coord, value));
             }
             return true;
         });
@@ -249,17 +259,17 @@ public:
         return result;
     }
 
-    auto get(const coord_type& coord) const noexcept -> value_type {
-        return {_cell_val(coord), typename value_type::from_cell_value_tag{}};
+    auto get(const coord_type& coord) const noexcept -> glyph_type {
+        return {_cell_val(coord), typename glyph_type::from_cell_value_tag{}};
     }
 
-    auto set(const coord_type& coord, value_type value) noexcept -> auto& {
+    auto set(const coord_type& coord, glyph_type value) noexcept -> auto& {
         _cell_ref(coord) = value.cell_value();
         return *this;
     }
 
     auto set_available_alternatives(const coord_type& coord) noexcept -> auto& {
-        value_type alternatives;
+        glyph_type alternatives;
         for(unsigned value = 0; value < glyph_count; ++value) {
             if(is_possible(coord, value)) {
                 alternatives.add(value);
@@ -293,7 +303,7 @@ public:
     template <typename Function>
     void for_each_alternative(const coord_type& coord, Function func) const {
         if(coord != invalid_coord()) {
-            get(coord).for_each_alternative([&](value_type alt) {
+            get(coord).for_each_alternative([&](glyph_type alt) {
                 auto candidate = This(*this).set(coord, alt);
                 if(candidate.is_possible(coord, alt)) {
                     if(!candidate.calculate_alternatives().has_empty()) {
@@ -302,6 +312,10 @@ public:
                 }
             });
         }
+    }
+
+    auto tight() const noexcept -> basic_sudoku_board<S, true> {
+        return {*this};
     }
 
 private:
@@ -325,25 +339,102 @@ private:
         return blocks[y * S + x];
     }
 
-    std::reference_wrapper<const board_type> _type;
+    friend class basic_sudoku_board<S, !Tight>;
+
+    std::reference_wrapper<const board_traits> _type;
     std::array<std::array<cell_type, glyph_count>, glyph_count> _blocks{};
 };
 //------------------------------------------------------------------------------
 template <unsigned S>
-auto basic_sudoku_board_type<S>::print(
+class basic_sudoku_board_generator {
+public:
+    using board_traits = basic_sudoku_board_traits<S>;
+    using board_type = basic_sudoku_board<S>;
+
+    basic_sudoku_board_generator(const board_traits& traits)
+      : _traits{traits} {}
+
+    auto generate(std::size_t count) -> board_type {
+        board_type result{_traits};
+        result.calculate_alternatives();
+
+        while(count) {
+            const typename board_type::coord_type coord{
+              _coord_dist(_rd),
+              _coord_dist(_rd),
+              _coord_dist(_rd),
+              _coord_dist(_rd)};
+
+            if(!result.get(coord).is_single()) {
+                while(true) {
+                    const auto value{_glyph_dist(_rd)};
+                    if(result.is_possible(coord, value)) {
+                        result.set(coord, value).calculate_alternatives();
+                        --count;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    auto generate_one() {
+        return generate(1);
+    }
+
+    auto generate_few() {
+        return generate(S + S);
+    }
+
+    auto generate_medium() {
+        return generate(S * S + S + S);
+    }
+
+    auto generate_many() {
+        return generate(S * S * S + S + S + S);
+    }
+
+private:
+    std::reference_wrapper<const board_traits> _traits;
+    std::random_device _rd;
+    std::uniform_int_distribution<unsigned> _coord_dist{0, S - 1};
+    std::uniform_int_distribution<unsigned> _glyph_dist{0, S* S - 1};
+};
+//------------------------------------------------------------------------------
+template <unsigned S>
+auto basic_sudoku_board_traits<S>::make_diagonal() -> board_type {
+    board_type result{*this};
+    unsigned g = 0;
+    for(unsigned b = 0; b < S; ++b) {
+        for(unsigned c = 0; c < S; ++c) {
+            result.set({{b, b, c, c}}, g++);
+        }
+    }
+    return result.calculate_alternatives();
+}
+//------------------------------------------------------------------------------
+template <unsigned S>
+auto basic_sudoku_board_traits<S>::make_generator() -> generator {
+    return {*this};
+}
+//------------------------------------------------------------------------------
+template <unsigned S>
+auto basic_sudoku_board_traits<S>::print(
   std::ostream& out,
-  const basic_sudoku_board<S>& board) const -> std::ostream& {
+  const basic_sudoku_board<S, false>& board) const -> std::ostream& {
     for(unsigned by = 0; by < S; ++by) {
         for(unsigned cy = 0; cy < S; ++cy) {
             for(unsigned bx = 0; bx < S; ++bx) {
                 for(unsigned cx = 0; cx < S; ++cx) {
                     const auto v = board.get({{bx, by, cx, cy}});
                     if(v.is_single()) {
-                        out << ' ' << _glyphs[v.get()];
+                        out << ' ' << _glyphs[v.get_index()];
                     } else if(v.is_empty()) {
                         out << ' ' << _empty_glyph;
                     } else {
-                        out << ' ' << _multi_glyph;
+                        out << ' ' << _multi_glyphs[(cx + cy) % S];
                     }
                 }
                 if(bx + 1 < S) {
@@ -372,20 +463,20 @@ auto basic_sudoku_board_type<S>::print(
 }
 //------------------------------------------------------------------------------
 template <unsigned S>
-auto basic_sudoku_board_type<S>::print_tight(
+auto basic_sudoku_board_traits<S>::print(
   std::ostream& out,
-  const basic_sudoku_board<S>& board) const -> std::ostream& {
+  const basic_sudoku_board<S, true>& board) const -> std::ostream& {
     for(unsigned by = 0; by < S; ++by) {
         for(unsigned cy = 0; cy < S; ++cy) {
             for(unsigned bx = 0; bx < S; ++bx) {
                 for(unsigned cx = 0; cx < S; ++cx) {
                     const auto v = board.get({{bx, by, cx, cy}});
                     if(v.is_single()) {
-                        out << _glyphs[v.get()];
+                        out << _glyphs[v.get_index()];
                     } else if(v.is_empty()) {
                         out << _empty_glyph;
                     } else {
-                        out << _multi_glyph;
+                        out << _multi_glyphs[(cx + cy) % S];
                     }
                 }
             }
@@ -394,58 +485,6 @@ auto basic_sudoku_board_type<S>::print_tight(
     }
     return out;
 }
-//------------------------------------------------------------------------------
-template <unsigned S>
-class basic_sudoku_board_generator {
-public:
-    using board_type = basic_sudoku_board_type<S>;
-
-    auto generate(const board_type& type, std::size_t count)
-      -> basic_sudoku_board<S> {
-        basic_sudoku_board<S> result{type};
-        result.calculate_alternatives();
-
-        while(count) {
-            const typename basic_sudoku_board<S>::coord_type coord{
-              _coord_dist(_rd),
-              _coord_dist(_rd),
-              _coord_dist(_rd),
-              _coord_dist(_rd)};
-
-            if(!result.get(coord).is_single()) {
-                while(true) {
-                    auto temp = result;
-                    const auto value{_glyph_dist(_rd)};
-                    if(temp.is_possible(coord, value)) {
-                        result =
-                          temp.set(coord, value).calculate_alternatives();
-                        --count;
-                        break;
-                    }
-                }
-            }
-        }
-
-        return result;
-    }
-
-    auto generate_few(const board_type& type) {
-        return generate(type, S + S);
-    }
-
-    auto generate_medium(const board_type& type) {
-        return generate(type, S * S + S + S);
-    }
-
-    auto generate_many(const board_type& type) {
-        return generate(type, S * S * S + S + S + S);
-    }
-
-private:
-    std::random_device _rd;
-    std::uniform_int_distribution<unsigned> _coord_dist{0, S - 1};
-    std::uniform_int_distribution<unsigned> _glyph_dist{0, S* S - 1};
-};
 //------------------------------------------------------------------------------
 } // namespace eagine
 
