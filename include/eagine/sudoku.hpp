9fe4811ee02a85fb9d63b2c3dad10b34a3f52b77
Matus Chochlik <chochlik@gmail.com>
2020-12-25 12:10:08 +0100

    Added default_sudoku_board_traits

--- include/eagine/sudoku.hpp
+++ include/eagine/sudoku.hpp
@@ -68,6 +68,67 @@ private:
 };
 //------------------------------------------------------------------------------
 template <unsigned S>
+class default_sudoku_board_traits;
+
+template <>
+class default_sudoku_board_traits<2> : public basic_sudoku_board_traits<2> {
+public:
+    // clang-format off
+	default_sudoku_board_traits() noexcept
+      : basic_sudoku_board_traits<2>{
+          {"1","2","3","4"},
+          { "?", "?"}, " "} {}
+    // clang-format on
+};
+
+template <>
+class default_sudoku_board_traits<3> : public basic_sudoku_board_traits<3> {
+public:
+    // clang-format off
+	default_sudoku_board_traits() noexcept
+      : basic_sudoku_board_traits<3>{
+          {"1","2","3","4","5","6","7","8","9"},
+          {"?", "?", "?"}, " "} {}
+    // clang-format on
+};
+
+template <>
+class default_sudoku_board_traits<4> : public basic_sudoku_board_traits<4> {
+public:
+    // clang-format off
+	default_sudoku_board_traits() noexcept
+      : basic_sudoku_board_traits<4>{
+          {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"},
+          {"?", "?", "?", "?"}, " "} {}
+    // clang-format on
+};
+
+template <>
+class default_sudoku_board_traits<5> : public basic_sudoku_board_traits<5> {
+public:
+    // clang-format off
+	default_sudoku_board_traits() noexcept
+      : basic_sudoku_board_traits<5>{
+          {"A","B","C","D","E","F","G","H","I","J","K","L","M"
+		  ,"N","O","P","Q","R","S","T","U","V","W","X","Y"},
+          {"?", "?", "?", "?", "?"}, " "} {}
+    // clang-format on
+};
+
+template <>
+class default_sudoku_board_traits<6> : public basic_sudoku_board_traits<6> {
+public:
+    // clang-format off
+	default_sudoku_board_traits() noexcept
+      : basic_sudoku_board_traits<6>{
+          {"0","1","2","3","4","5","6","7","8","9","A","B"
+		  ,"C","D","E","F","G","H","I","J","K","L","M","N"
+		  ,"O","P","Q","R","S","T","U","V","W","X","Y","Z"},
+          {"?", "?", "?", "?", "?", "?"}, " "} {}
+    // clang-format on
+};
+//------------------------------------------------------------------------------
+template <unsigned S>
 class basic_sudoku_glyph {
 public:
     using board_traits = basic_sudoku_board_traits<S>;
