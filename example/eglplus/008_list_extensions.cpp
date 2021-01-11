60d0c0e8cc4c3a2d80d86d4a85a2d04a60e2fa1d
Matus Chochlik <chochlik@gmail.com>
2021-01-11 07:57:23 +0100

    Refactoring in EGL examples

---
+++ example/eglplus/008_list_extensions.cpp
@@ -0,0 +1,94 @@
+/**
+ *  example eglplus/008_list_extensions.cpp
+ *
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#include <eagine/integer_range.hpp>
+#include <eglplus/egl.hpp>
+#include <eglplus/egl_api.hpp>
+#include <iostream>
+#include <vector>
+
+auto main() -> int {
+    using namespace eagine;
+    using namespace eagine::eglp;
+
+    egl_api egl;
+
+    std::cout << "Generic extensions: " << std::endl;
+
+    if(ok extensions{egl.get_extensions()}) {
+        for(auto name : extensions) {
+            std::cout << "  " << name << std::endl;
+        }
+    } else {
+        std::cerr << "failed to get extension list: " << (!extensions).message()
+                  << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    if(ok dev_count{egl.query_devices.count()}) {
+        std::vector<egl_types::device_type> devices;
+        const auto n = std_size(dev_count.get());
+        devices.resize(n);
+        if(egl.query_devices(cover(devices))) {
+            for(auto d : integer_range(n)) {
+                std::cout << "Device: " << d << std::endl;
+
+                std::cout << " Device extensions: " << std::endl;
+                if(ok extensions{egl.get_device_extensions(devices[d])}) {
+                    for(auto name : extensions) {
+                        std::cout << "  " << name << std::endl;
+                    }
+                } else {
+                    std::cerr << "failed to get device extension list: "
+                              << (!extensions).message() << std::endl;
+                }
+
+                if(egl.get_platform_display) {
+                    if(ok display{egl.get_platform_display(
+                         egl.platform_device, devices[d])}) {
+                        if(auto init_res = egl.initialize(display)) {
+                            auto do_cleanup = egl.terminate.raii(display);
+
+                            if(ok vendor{
+                                 egl.query_string(display, egl.vendor)}) {
+                                std::cout << " Vendor:  " << vendor
+                                          << std::endl;
+                            }
+
+                            if(ok version{
+                                 egl.query_string(display, egl.version)}) {
+                                std::cout << " Version: " << version
+                                          << std::endl;
+                            }
+
+                            std::cout << " Display extensions: " << std::endl;
+
+                            if(ok extensions = egl.get_extensions(display)) {
+                                for(auto name : extensions) {
+                                    std::cout << "  " << name << std::endl;
+                                }
+                            } else {
+                                std::cerr << "failed to get extension list: "
+                                          << (!extensions).message()
+                                          << std::endl;
+                            }
+                        } else {
+                            std::cout << "missing required API function."
+                                      << std::endl;
+                        }
+                    }
+                }
+                std::cout << std::endl;
+            }
+        }
+    }
+
+    return 0;
+}
