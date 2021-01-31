150a145989b8da71a52343f4faa71c8cb042d0e0
Matus Chochlik <chochlik@gmail.com>
2021-01-31 11:43:59 +0100

    Refactored a combined example into an application example

--- example/application/019_scaled_cubes/resources.cpp
+++ example/application/019_scaled_cubes/resources.cpp
@@ -1,6 +1,4 @@
 /**
- *  example combined/019_scaled_cubes/resources.cpp
- *
  *  Copyright Matus Chochlik.
  *  Distributed under the Boost Software License, Version 1.0.
  *  See accompanying file LICENSE_1_0.txt or copy at
@@ -8,9 +6,10 @@
  */
 
 #include "resources.hpp"
-#include "../example.hpp"
-#include "../example/camera.hpp"
 
+#include <eagine/application/camera.hpp>
+#include <eagine/application/context.hpp>
+#include <eagine/embed.hpp>
 #include <eagine/shapes/array.hpp>
 #include <eagine/shapes/centered.hpp>
 #include <eagine/shapes/cube.hpp>
@@ -19,19 +18,17 @@
 #include <oglplus/math/interpolate.hpp>
 #include <oglplus/shapes/generator.hpp>
 
-namespace eagine::oglp {
+namespace eagine::application {
 //------------------------------------------------------------------------------
 // program
 //------------------------------------------------------------------------------
-void cubes_program::init(example_context& ctx) {
-    auto& cleanup = ctx.cleanup();
-    auto& gl = ctx.gl();
+void cubes_program::init(execution_context& ec, video_context& vc) {
+    auto& gl = vc.gl_api();
 
     gl.create_program() >> prog;
-    gl.delete_program.later_by(cleanup, prog);
 
     const auto prog_src{embed(EAGINE_ID(prog), "example.oglpprog")};
-    gl.build_program(prog, prog_src.unpack(ctx.main()));
+    gl.build_program(prog, prog_src.unpack(ec));
     gl.use_program(prog);
 
     gl.get_uniform_location(prog, "Camera") >> camera_loc;
@@ -40,58 +37,63 @@ void cubes_program::init(example_context& ctx) {
     gl.get_uniform_location(prog, "Edges") >> edges_loc;
 }
 //------------------------------------------------------------------------------
-void cubes_program::set_projection(
-  const example_context& ctx,
-  const example_orbiting_camera& camera) {
-    ctx.gl().set_uniform(prog, camera_loc, camera.matrix(ctx.state()));
+void cubes_program::cleanup(video_context& vc) {
+    auto& gl = vc.gl_api();
+    gl.delete_program(std::move(prog));
 }
 //------------------------------------------------------------------------------
-void cubes_program::update(const example_context& ctx) {
-    const auto t = ctx.state().exec_time().value();
-    ctx.gl().set_uniform(
+void cubes_program::set_projection(video_context& vc, orbiting_camera& camera) {
+    vc.gl_api().set_uniform(
+      prog, camera_loc, camera.matrix(vc.surface_aspect()));
+}
+//------------------------------------------------------------------------------
+void cubes_program::update(execution_context& ec, video_context& vc) {
+    const auto t = ec.state().frame_time().value();
+    const auto& glapi = vc.gl_api();
+    glapi.set_uniform(
       prog,
       center_loc,
-      to_cartesian(unit_spherical_coordinates(
+      oglp::to_cartesian(oglp::unit_spherical_coordinates(
         turns_(t / 3.F),
-        smooth_lerp(right_angles_(1.F), right_angles_(-1.F), t / 5.F))) *
-        smooth_lerp(0.F, 10.F, t / 7.F));
-    ctx.gl().set_uniform(prog, time_loc, t);
+        oglp::smooth_lerp(right_angles_(1.F), right_angles_(-1.F), t / 5.F))) *
+        oglp::smooth_lerp(0.F, 10.F, t / 7.F));
+    glapi.set_uniform(prog, time_loc, t);
 }
 //------------------------------------------------------------------------------
 void cubes_program::bind_position_location(
-  const example_context& ctx,
-  vertex_attrib_location loc) {
-    ctx.gl().bind_attrib_location(prog, loc, "Position");
+  video_context& vc,
+  oglp::vertex_attrib_location loc) {
+    vc.gl_api().bind_attrib_location(prog, loc, "Position");
 }
 //------------------------------------------------------------------------------
 void cubes_program::bind_pivot_location(
-  const example_context& ctx,
-  vertex_attrib_location loc) {
-    ctx.gl().bind_attrib_location(prog, loc, "Pivot");
+  video_context& vc,
+  oglp::vertex_attrib_location loc) {
+    vc.gl_api().bind_attrib_location(prog, loc, "Pivot");
 }
 //------------------------------------------------------------------------------
 void cubes_program::bind_coord_location(
-  const example_context& ctx,
-  vertex_attrib_location loc) {
-    ctx.gl().bind_attrib_location(prog, loc, "Coord");
+  video_context& vc,
+  oglp::vertex_attrib_location loc) {
+    vc.gl_api().bind_attrib_location(prog, loc, "Coord");
 }
 //------------------------------------------------------------------------------
-void cubes_program::drawing_surface(const example_context& ctx) {
-    ctx.gl().set_uniform(prog, edges_loc, 0.F);
+void cubes_program::drawing_surface(video_context& vc) {
+    vc.gl_api().set_uniform(prog, edges_loc, 0.F);
 }
 //------------------------------------------------------------------------------
-void cubes_program::drawing_edges(const example_context& ctx) {
-    ctx.gl().set_uniform(prog, edges_loc, 1.F);
+void cubes_program::drawing_edges(video_context& vc) {
+    vc.gl_api().set_uniform(prog, edges_loc, 1.F);
 }
 //------------------------------------------------------------------------------
 // geometry
 //------------------------------------------------------------------------------
-void cubes_geometry::init(example_context& ctx) {
-    auto& cleanup = ctx.cleanup();
-    const auto& gl = ctx.gl();
+void cubes_geometry::init(execution_context& ec, video_context& vc) {
+    const auto& glapi = vc.gl_api();
+    const auto& gl = glapi;
 
-    shape_generator shape(
-      gl,
+    oglp::shape_generator shape(
+      glapi,
       shapes::rebox(shapes::center(eagine::shapes::ortho_array_xyz(
         shapes::unit_cube(
           shapes::vertex_attrib_kind::position |
@@ -104,58 +106,62 @@ void cubes_geometry::init(example_context& ctx) {
       shape.draw_variant(0), shape.draw_variant(1)};
 
     ops.resize(std_size(shape.operation_count(view(vars))));
-    shape.instructions(ctx.gl(), view(vars), cover(subs), cover(ops));
+    shape.instructions(glapi, view(vars), cover(subs), cover(ops));
 
     // vao
     gl.gen_vertex_arrays() >> vao;
-    gl.delete_vertex_arrays.later_by(cleanup, vao);
     gl.bind_vertex_array(vao);
 
     // positions
     gl.gen_buffers() >> positions;
-    gl.delete_buffers.later_by(cleanup, positions);
     shape.attrib_setup(
-      ctx.gl(),
+      glapi,
       vao,
       positions,
       position_loc(),
       eagine::shapes::vertex_attrib_kind::position,
-      ctx.buffer());
+      ec.buffer());
 
     // pivots
     gl.gen_buffers() >> pivots;
-    gl.delete_buffers.later_by(cleanup, pivots);
     shape.attrib_setup(
-      ctx.gl(),
+      glapi,
       vao,
       pivots,
       pivot_loc(),
       eagine::shapes::vertex_attrib_kind::pivot,
-      ctx.buffer());
+      ec.buffer());
 
     // coords
     gl.gen_buffers() >> coords;
-    gl.delete_buffers.later_by(cleanup, coords);
     shape.attrib_setup(
-      ctx.gl(),
+      glapi,
       vao,
       coords,
       coord_loc(),
       eagine::shapes::vertex_attrib_kind::box_coord,
-      ctx.buffer());
+      ec.buffer());
 
     // indices
     gl.gen_buffers() >> indices;
-    gl.delete_buffers.later_by(cleanup, indices);
-    shape.index_setup(ctx.gl(), indices, view(vars), ctx.buffer());
+    shape.index_setup(glapi, indices, view(vars), ec.buffer());
 }
 //------------------------------------------------------------------------------
-void cubes_geometry::draw_surface(const example_context& ctx) {
-    draw_using_instructions(ctx.gl(), view(ops), subs[0]);
+void cubes_geometry::cleanup(video_context& vc) {
+    auto& gl = vc.gl_api();
+    gl.delete_buffers(std::move(indices));
+    gl.delete_buffers(std::move(coords));
+    gl.delete_buffers(std::move(pivots));
+    gl.delete_buffers(std::move(positions));
+    gl.delete_vertex_arrays(std::move(vao));
 }
 //------------------------------------------------------------------------------
-void cubes_geometry::draw_edges(const example_context& ctx) {
-    draw_using_instructions(ctx.gl(), view(ops), subs[1]);
+void cubes_geometry::draw_surface(video_context& vc) {
+    draw_using_instructions(vc.gl_api(), view(ops), subs[0]);
 }
 //------------------------------------------------------------------------------
-} // namespace eagine::oglp
+void cubes_geometry::draw_edges(video_context& vc) {
+    draw_using_instructions(vc.gl_api(), view(ops), subs[1]);
+}
+//------------------------------------------------------------------------------
+} // namespace eagine::application
---
+++ example/application/019_scaled_cubes/resources.hpp
@@ -0,0 +1,79 @@
+/**
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#ifndef OGLPLUS_EXAMPLE_RESOURCES_HPP // NOLINT(llvm-header-guard)
+#define OGLPLUS_EXAMPLE_RESOURCES_HPP
+
+#include <oglplus/gl.hpp>
+#include <oglplus/gl_api.hpp>
+
+#include <eagine/application/fwd.hpp>
+#include <oglplus/shapes/drawing.hpp>
+#include <array>
+
+namespace eagine::application {
+//------------------------------------------------------------------------------
+// program
+//------------------------------------------------------------------------------
+class cubes_program {
+private:
+    oglp::owned_program_name prog;
+    oglp::uniform_location camera_loc;
+    oglp::uniform_location center_loc;
+    oglp::uniform_location time_loc;
+    oglp::uniform_location edges_loc;
+
+public:
+    void init(execution_context&, video_context&);
+    void cleanup(video_context&);
+    void set_projection(video_context&, orbiting_camera& camera);
+    void update(execution_context&, video_context&);
+
+    void bind_position_location(video_context&, oglp::vertex_attrib_location);
+    void bind_pivot_location(video_context&, oglp::vertex_attrib_location);
+    void bind_coord_location(video_context&, oglp::vertex_attrib_location);
+
+    void drawing_surface(video_context&);
+    void drawing_edges(video_context&);
+};
+//------------------------------------------------------------------------------
+// geometry
+//------------------------------------------------------------------------------
+class cubes_geometry {
+private:
+    oglp::owned_vertex_array_name vao;
+
+    oglp::owned_buffer_name positions;
+    oglp::owned_buffer_name pivots;
+    oglp::owned_buffer_name coords;
+    oglp::owned_buffer_name indices;
+
+    std::vector<oglp::shape_draw_operation> ops{};
+    std::array<oglp::shape_draw_subset, 2> subs{};
+
+public:
+    void init(execution_context&, video_context&);
+    void cleanup(video_context&);
+    void draw_surface(video_context&);
+    void draw_edges(video_context&);
+
+    static auto position_loc() noexcept {
+        return oglp::vertex_attrib_location{0};
+    }
+
+    static auto pivot_loc() noexcept {
+        return oglp::vertex_attrib_location{1};
+    }
+
+    static auto coord_loc() noexcept {
+        return oglp::vertex_attrib_location{2};
+    }
+};
+//------------------------------------------------------------------------------
+} // namespace eagine::application
+
+#endif
