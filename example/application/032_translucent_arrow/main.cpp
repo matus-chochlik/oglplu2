042b6c960eb9a9e4d241e9185491143add4919aa
Matus Chochlik <chochlik@gmail.com>
2021-02-01 07:34:35 +0100

    Refactored a combined example into an application example

---
+++ example/application/032_translucent_arrow/main.cpp
@@ -0,0 +1,165 @@
+/**
+ *  Copyright Matus Chochlik.
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  See accompanying file LICENSE_1_0.txt or copy at
+ *   http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#include <oglplus/gl.hpp>
+#include <oglplus/gl_api.hpp>
+
+#include <eagine/units/unit/si/angle.hpp>
+//
+#include <eagine/application/camera.hpp>
+#include <eagine/application/main.hpp>
+#include <eagine/cleanup_group.hpp>
+#include <eagine/timeout.hpp>
+#include <oglplus/math/matrix.hpp>
+#include <oglplus/math/vector.hpp>
+
+#include "resources.hpp"
+
+namespace eagine::application {
+//------------------------------------------------------------------------------
+class example_arrow : public application {
+public:
+    example_arrow(execution_context&, video_context&);
+
+    auto is_done() noexcept -> bool final {
+        return _is_done.is_expired();
+    }
+
+    void on_video_resize() noexcept final;
+    void update() noexcept final;
+    void cleanup() noexcept final;
+
+private:
+    cleanup_group _cleanup;
+    execution_context& _ctx;
+    video_context& _video;
+    timeout _is_done{std::chrono::seconds{30}};
+
+    orbiting_camera camera;
+
+    depth_program depth_prog;
+    draw_program draw_prog;
+    arrow_geometry arrow;
+    depth_texture depth_tex;
+};
+//------------------------------------------------------------------------------
+example_arrow::example_arrow(execution_context& ec, video_context& vc)
+  : _ctx{ec}
+  , _video{vc} {
+    auto& glapi = _video.gl_api();
+    auto& [gl, GL] = glapi;
+
+    arrow.init(ec, vc, _cleanup);
+    depth_tex.init(ec, vc, _cleanup);
+
+    depth_prog.init(ec, vc, _cleanup);
+    depth_prog.bind_position_location(vc, arrow.position_loc());
+
+    draw_prog.init(ec, vc, _cleanup);
+    draw_prog.bind_position_location(vc, arrow.position_loc());
+    draw_prog.bind_normal_location(vc, arrow.normal_loc());
+    draw_prog.set_depth_texture(vc, depth_tex.texture_unit());
+
+    // camera
+    const auto sr = arrow.bounding_sphere().radius();
+    camera.set_near(sr * 0.1F)
+      .set_far(sr * 4.0F)
+      .set_orbit_min(sr * 1.2F)
+      .set_orbit_max(sr * 1.7F)
+      .set_fov(degrees_(80.F));
+    depth_prog.set_projection(vc, camera);
+    draw_prog.set_projection(vc, camera);
+
+    gl.clear_color(0.45F, 0.45F, 0.45F, 0.0F);
+    gl.enable(GL.depth_test);
+    gl.enable(GL.cull_face);
+    gl.cull_face(GL.back);
+
+    camera.connect_inputs(ec).basic_input_mapping(ec);
+    ec.setup_inputs().switch_input_mapping();
+}
+//------------------------------------------------------------------------------
+void example_arrow::on_video_resize() noexcept {
+    auto& gl = _video.gl_api();
+    gl.viewport(_video.surface_size());
+}
+//------------------------------------------------------------------------------
+void example_arrow::update() noexcept {
+    if(!_ctx.state().user_is_idle()) {
+        _is_done.reset();
+    }
+
+    auto& glapi = _video.gl_api();
+    auto& [gl, GL] = glapi;
+
+    gl.clear_depth(0);
+    gl.clear(GL.depth_buffer_bit);
+    gl.depth_func(GL.greater);
+    gl.cull_face(GL.front);
+    depth_prog.update(_video);
+    depth_prog.set_projection(_video, camera);
+    arrow.draw(_video);
+
+    depth_tex.copy_from_fb(_video);
+
+    gl.clear_depth(1);
+    gl.clear(GL.color_buffer_bit | GL.depth_buffer_bit);
+    gl.depth_func(GL.less);
+    gl.cull_face(GL.back);
+    draw_prog.update(_ctx, _video);
+    draw_prog.set_projection(_video, camera);
+    arrow.draw(_video);
+
+    _video.commit();
+}
+//------------------------------------------------------------------------------
+void example_arrow::cleanup() noexcept {
+    _cleanup.clear();
+
+    _video.end();
+}
+//------------------------------------------------------------------------------
+class example_launchpad : public launchpad {
+public:
+    auto setup(main_ctx&, launch_options& opts) -> bool final {
+        opts.no_audio().require_input().require_video();
+        return true;
+    }
+
+    auto check_requirements(video_context& vc) -> bool {
+        auto& [gl, GL] = vc.gl_api();
+
+        return gl.disable && gl.clear_color && gl.create_shader &&
+               gl.shader_source && gl.compile_shader && gl.create_program &&
+               gl.attach_shader && gl.link_program && gl.use_program &&
+               gl.gen_buffers && gl.bind_buffer && gl.buffer_data &&
+               gl.gen_vertex_arrays && gl.bind_vertex_array &&
+               gl.get_attrib_location && gl.vertex_attrib_pointer &&
+               gl.enable_vertex_attrib_array && gl.draw_arrays &&
+               GL.vertex_shader && GL.fragment_shader;
+    }
+
+    auto launch(execution_context& ec, const launch_options&)
+      -> std::unique_ptr<application> final {
+        if(auto opt_vc{ec.video_ctx()}) {
+            auto& vc = extract(opt_vc);
+            vc.begin();
+            if(vc.init_gl_api()) {
+                if(check_requirements(vc)) {
+                    return {std::make_unique<example_arrow>(ec, vc)};
+                }
+            }
+        }
+        return {};
+    }
+};
+//------------------------------------------------------------------------------
+auto establish(main_ctx&) -> std::unique_ptr<launchpad> {
+    return {std::make_unique<example_launchpad>()};
+}
+//------------------------------------------------------------------------------
+} // namespace eagine::application
--- example/application/032_translucent_arrow/resources.cpp
+++ example/application/032_translucent_arrow/resources.cpp
@@ -1,6 +1,4 @@
 /**
- *  example combined/032_translucent_arrow/resources.cpp
- *
  *  Copyright Matus Chochlik.
  *  Distributed under the Boost Software License, Version 1.0.
  *  See accompanying file LICENSE_1_0.txt or copy at
@@ -8,27 +6,31 @@
  */
 
 #include "resources.hpp"
-#include "../example.hpp"
-#include "../example/camera.hpp"
 
+#include <eagine/application/camera.hpp>
+#include <eagine/application/context.hpp>
+#include <eagine/embed.hpp>
 #include <eagine/shapes/value_tree.hpp>
 #include <eagine/value_tree/json.hpp>
 #include <oglplus/glsl/string_ref.hpp>
 #include <oglplus/shapes/generator.hpp>
 
-namespace eagine::oglp {
+namespace eagine::application {
 //------------------------------------------------------------------------------
 // programs
 //------------------------------------------------------------------------------
-void depth_program::init(example_context& ctx) {
-    auto& cleanup = ctx.cleanup();
-    const auto& [gl, GL] = ctx.gl();
+void depth_program::init(
+  execution_context& ec,
+  video_context& vc,
+  cleanup_group& cleanup) {
+    const auto& [gl, GL] = vc.gl_api();
 
     // vertex shader
-    owned_shader_name vs;
+    oglp::owned_shader_name vs;
     auto vs_src = embed(EAGINE_ID(VSDepth), "vertex_depth.glsl");
     gl.create_shader(GL.vertex_shader) >> vs;
-    gl.shader_source(vs, glsl_string_ref(vs_src));
+    auto cleanup_vs = gl.delete_shader.raii(vs);
+    gl.shader_source(vs, oglp::glsl_string_ref(vs_src.unpack(ec)));
     gl.compile_shader(vs);
 
     // program
@@ -41,43 +43,43 @@ void depth_program::init(example_context& ctx) {
     gl.get_uniform_location(prog, "Projection") >> camera_loc;
 }
 //------------------------------------------------------------------------------
-void depth_program::set_projection(
-  const example_context& ctx,
-  const example_orbiting_camera& camera) {
-    auto& state = ctx.state();
-    const auto& gl = ctx.gl();
+void depth_program::set_projection(video_context& vc, orbiting_camera& camera) {
+    const auto& gl = vc.gl_api();
 
     gl.use_program(prog);
-    gl.set_uniform(prog, camera_loc, camera.matrix(state));
+    gl.set_uniform(prog, camera_loc, camera.matrix(vc));
 }
 //------------------------------------------------------------------------------
-void depth_program::update(const example_context& ctx) {
-    const auto& gl = ctx.gl();
-    gl.use_program(prog);
+void depth_program::update(video_context& vc) {
+    vc.gl_api().use_program(prog);
 }
 //------------------------------------------------------------------------------
 void depth_program::bind_position_location(
-  const example_context& ctx,
-  vertex_attrib_location loc) {
-    ctx.gl().bind_attrib_location(prog, loc, "Position");
+  video_context& vc,
+  oglp::vertex_attrib_location loc) {
+    vc.gl_api().bind_attrib_location(prog, loc, "Position");
 }
 //------------------------------------------------------------------------------
-void draw_program::init(example_context& ctx) {
-    auto& cleanup = ctx.cleanup();
-    const auto& [gl, GL] = ctx.gl();
+void draw_program::init(
+  execution_context& ec,
+  video_context& vc,
+  cleanup_group& cleanup) {
+    const auto& [gl, GL] = vc.gl_api();
 
     // vertex shader
-    owned_shader_name vs;
+    oglp::owned_shader_name vs;
     auto vs_src = embed(EAGINE_ID(VSDraw), "vertex_draw.glsl");
     gl.create_shader(GL.vertex_shader) >> vs;
-    gl.shader_source(vs, glsl_string_ref(vs_src));
+    auto cleanup_vs = gl.delete_shader.raii(vs);
+    gl.shader_source(vs, oglp::glsl_string_ref(vs_src.unpack(ec)));
     gl.compile_shader(vs);
 
     // fragment shader
-    owned_shader_name fs;
+    oglp::owned_shader_name fs;
     auto fs_src = embed(EAGINE_ID(FSDraw), "fragment_draw.glsl");
     gl.create_shader(GL.fragment_shader) >> fs;
-    gl.shader_source(fs, glsl_string_ref(fs_src));
+    auto cleanup_fs = gl.delete_shader.raii(fs);
+    gl.shader_source(fs, oglp::glsl_string_ref(fs_src.unpack(ec)));
     gl.compile_shader(fs);
 
     // program
@@ -94,67 +96,66 @@ void draw_program::init(example_context& ctx) {
 }
 //------------------------------------------------------------------------------
 void draw_program::set_depth_texture(
-  const example_context& ctx,
-  gl_types::int_type tex_unit) {
-    const auto& gl = ctx.gl();
+  video_context& vc,
+  oglp::gl_types::int_type tex_unit) {
+    const auto& gl = vc.gl_api();
 
     gl.use_program(prog);
     gl.set_uniform(prog, depth_tex_loc, tex_unit);
 }
 //------------------------------------------------------------------------------
-void draw_program::set_projection(
-  const example_context& ctx,
-  const example_orbiting_camera& camera) {
-    auto& state = ctx.state();
-    const auto& gl = ctx.gl();
+void draw_program::set_projection(video_context& vc, orbiting_camera& camera) {
+    const auto& gl = vc.gl_api();
 
     gl.use_program(prog);
-    gl.set_uniform(prog, camera_loc, camera.matrix(state));
+    gl.set_uniform(prog, camera_loc, camera.matrix(vc));
 }
 //------------------------------------------------------------------------------
-void draw_program::update(const example_context& ctx) {
-    const auto& gl = ctx.gl();
+void draw_program::update(execution_context& ec, video_context& vc) {
+    const auto& gl = vc.gl_api();
 
-    rad += radians_(0.5F * ctx.state().frame_duration().value());
+    rad += radians_(0.5F * ec.state().frame_duration().value());
 
     gl.use_program(prog);
     gl.set_uniform(
       prog,
       light_pos_loc,
-      vec3(cos(rad) * 5, sin(rad) * 7, sin(rad * 0.618F) * 8));
+      oglp::vec3(cos(rad) * 5, sin(rad) * 7, sin(rad * 0.618F) * 8));
 }
 //------------------------------------------------------------------------------
 void draw_program::bind_position_location(
-  const example_context& ctx,
-  vertex_attrib_location loc) {
-    ctx.gl().bind_attrib_location(prog, loc, "Position");
+  video_context& vc,
+  oglp::vertex_attrib_location loc) {
+    vc.gl_api().bind_attrib_location(prog, loc, "Position");
 }
 //------------------------------------------------------------------------------
 void draw_program::bind_normal_location(
-  const example_context& ctx,
-  vertex_attrib_location loc) {
-    ctx.gl().bind_attrib_location(prog, loc, "Normal");
+  video_context& vc,
+  oglp::vertex_attrib_location loc) {
+    vc.gl_api().bind_attrib_location(prog, loc, "Normal");
 }
 //------------------------------------------------------------------------------
 // geometry
 //------------------------------------------------------------------------------
-void arrow_geometry::init(example_context& ctx) {
-    auto& cleanup = ctx.cleanup();
-    const auto& [gl, GL] = ctx.gl();
+void arrow_geometry::init(
+  execution_context& ec,
+  video_context& vc,
+  cleanup_group& cleanup) {
+    const auto& glapi = vc.gl_api();
+    const auto& [gl, GL] = glapi;
 
     auto load_shape_data = [&]() {
-        const auto json_text =
-          as_chars(embed(EAGINE_ID(ArrowJson), "arrow.json"));
-        return valtree::from_json_text(json_text, ctx.main());
+        return valtree::from_json_text(
+          as_chars(embed(EAGINE_ID(ArrowJson), "arrow.json").unpack(ec)), ec);
     };
 
-    shape_generator shape(
-      ctx.gl(), shapes::from_value_tree(load_shape_data(), ctx.main()));
+    oglp::shape_generator shape(
+      glapi, shapes::from_value_tree(load_shape_data(), ec));
 
     bound_sphere = shape.bounding_sphere();
 
     ops.resize(std_size(shape.operation_count()));
-    shape.instructions(ctx.gl(), cover(ops));
+    shape.instructions(glapi, cover(ops));
 
     // vao
     gl.gen_vertex_arrays() >> vao;
@@ -165,52 +166,53 @@ void arrow_geometry::init(example_context& ctx) {
     gl.gen_buffers() >> positions;
     gl.delete_buffers.later_by(cleanup, positions);
     shape.attrib_setup(
-      ctx.gl(),
+      glapi,
       vao,
       positions,
       position_loc(),
       eagine::shapes::vertex_attrib_kind::position,
-      ctx.buffer());
+      ec.buffer());
 
     // normals
     gl.gen_buffers() >> normals;
     gl.delete_buffers.later_by(cleanup, normals);
     shape.attrib_setup(
-      ctx.gl(),
+      glapi,
       vao,
       normals,
       normal_loc(),
       eagine::shapes::vertex_attrib_kind::normal,
-      ctx.buffer());
+      ec.buffer());
 
     // indices
     gl.gen_buffers() >> indices;
     gl.delete_buffers.later_by(cleanup, indices);
-    shape.index_setup(ctx.gl(), indices, ctx.buffer());
+    shape.index_setup(glapi, indices, ec.buffer());
 
     gl.enable(GL.depth_test);
     gl.enable(GL.cull_face);
     gl.cull_face(GL.back);
 }
 //------------------------------------------------------------------------------
-void arrow_geometry::draw(const example_context& ctx) {
-    draw_using_instructions(ctx.gl(), view(ops));
+void arrow_geometry::draw(video_context& vc) {
+    draw_using_instructions(vc.gl_api(), view(ops));
 }
 //------------------------------------------------------------------------------
 // texture
 //------------------------------------------------------------------------------
-void depth_texture::init(example_context& ctx) {
-    auto& cleanup = ctx.cleanup();
-    const auto& gl = ctx.gl();
+void depth_texture::init(
+  execution_context&,
+  video_context& vc,
+  cleanup_group& cleanup) {
+    const auto& gl = vc.gl_api();
 
     gl.gen_textures() >> tex;
     gl.delete_textures.later_by(cleanup, tex);
 }
 //------------------------------------------------------------------------------
-void depth_texture::reshape(const example_context& ctx) {
-    const auto& [gl, GL] = ctx.gl();
-    const auto width = ctx.state().width();
-    const auto height = ctx.state().height();
+void depth_texture::reshape(video_context& vc) {
+    const auto& [gl, GL] = vc.gl_api();
+    const auto [width, height] = vc.surface_size();
 
     gl.active_texture(GL.texture0 + tex_unit);
     gl.bind_texture(GL.texture_rectangle, tex);
@@ -231,19 +233,13 @@ void depth_texture::reshape(const example_context& ctx) {
     gl.clear(GL.depth_buffer_bit);
 }
 //------------------------------------------------------------------------------
-void depth_texture::copy_from_fb(const example_context& ctx) {
-    const auto& [gl, GL] = ctx.gl();
+void depth_texture::copy_from_fb(video_context& vc) {
+    const auto [width, height] = vc.surface_size();
+    const auto& [gl, GL] = vc.gl_api();
 
     gl.copy_tex_image2d(
-      GL.texture_rectangle,
-      0,
-      GL.depth_component,
-      0,
-      0,
-      ctx.state().width(),
-      ctx.state().height(),
-      0);
+      GL.texture_rectangle, 0, GL.depth_component, 0, 0, width, height, 0);
 }
 //------------------------------------------------------------------------------
 
-} // namespace eagine::oglp
+} // namespace eagine::application
