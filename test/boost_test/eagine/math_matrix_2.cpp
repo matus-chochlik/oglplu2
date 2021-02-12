aea2b6734b8c6b630c848e735fde68bb2d6855a2
Matus Chochlik <chochlik@gmail.com>
2021-02-12 21:28:30 +0100

    Replaced C style comment with C++ comments in source file headings

--- test/boost_test/eagine/math_matrix_2.cpp
+++ test/boost_test/eagine/math_matrix_2.cpp
@@ -1,9 +1,8 @@
-/**
- *  Copyright Matus Chochlik.
- *  Distributed under the Boost Software License, Version 1.0.
- *  See accompanying file LICENSE_1_0.txt or copy at
- *   http://www.boost.org/LICENSE_1_0.txt
- */
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
 #include <eagine/math/matrix.hpp>
 #include <utility>
 #define BOOST_TEST_MODULE EAGINE_math_matrix_2
--- test/boost_test/eagine/zip_iterator.cpp
+++ test/boost_test/eagine/zip_iterator.cpp
@@ -1,9 +1,8 @@
-/**
- *  Copyright Matus Chochlik.
- *  Distributed under the Boost Software License, Version 1.0.
- *  See accompanying file LICENSE_1_0.txt or copy at
- *   http://www.boost.org/LICENSE_1_0.txt
- */
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
 #include <eagine/zip_iterator.hpp>
 #define BOOST_TEST_MODULE EAGINE_zip_iterator
 #include "../unit_test_begin.inl"
