2955c7fa5d7fee8c1ff110dd0ec478f5b69a4cf4
Matus Chochlik <chochlik@gmail.com>
2021-04-03 15:08:20 +0200

    Additions to the sudoku development / debugging helper script

--- devel/sudoku.sh
+++ devel/sudoku.sh
@@ -14,16 +14,39 @@ pids=()
 ${install_prefix}/bin/eagine-message_bus-router \
 	"${log_args[@]}" \
 	"${conn_type}" \
+	--msg-bus-router-address /tmp/sudoku_t \
+	--msg-bus-router-id-major 0 \
+	--msg-bus-router-id-count 500 \
 	--msg-bus-router-shutdown-verify false \
 	--msg-bus-router-shutdown-when-idle true \
 	--msg-bus-router-shutdown-delay 20s \
 	& termpids+=($!)
 sleep 1
+${install_prefix}/bin/eagine-message_bus-router \
+	"${log_args[@]}" \
+	"${conn_type}" \
+	--msg-bus-router-address /tmp/sudoku_h \
+	--msg-bus-router-id-major 500 \
+	--msg-bus-router-id-count 500 \
+	--msg-bus-router-shutdown-verify false \
+	--msg-bus-router-shutdown-when-idle true \
+	--msg-bus-router-shutdown-delay 20s \
+	& termpids+=($!)
+sleep 5
+${install_prefix}/bin/eagine-local-bridge \
+	"${log_args[@]}" \
+	${conn_type} \
+	-c --msg-bus-bridge-shutdown-verify false \
+	-l --msg-bus-router-address /tmp/sudoku_t \
+	-r --msg-bus-router-address /tmp/sudoku_h \
+	& pids+=($!)
+sleep 1
 ${install_prefix}/bin/eagine-message_bus-sudoku_helper \
 	"${log_args[@]}" \
 	"${conn_type}" \
+	--msg-bus-router-address /tmp/sudoku_h \
 	--msg-bus-router-id-major 1000 \
-	--msg-bus-router-id-count 1000 \
+	--msg-bus-router-id-count 500 \
 	& termpids+=($!)
 sleep 1
 rank=4
@@ -31,6 +54,7 @@ div=$((rank * (rank - 2)))
 ${install_prefix}/bin/eagine-message_bus-sudoku_tiling \
 	"${log_args[@]}" \
 	"${conn_type}" \
+	--msg-bus-router-address /tmp/sudoku_t \
 	--msg-bus-sudoku-solver-block-cells true \
 	--msg-bus-sudoku-solver-print-incomplete true \
 	--${rank} \
@@ -43,6 +67,7 @@ do
 	"${install_prefix}/bin/eagine-message_bus-bridge" \
 		"${log_args[@]}" \
 		"${conn_type}" \
+		--msg-bus-router-address /tmp/sudoku_t \
 		--msg-bus-bridge-shutdown-delay 15s \
 		--ssh "${ssh_host}" \
 	&  termpids+=($!)
@@ -50,9 +75,10 @@ done
 sleep 10
 ${install_prefix}/bin/eagine-message_bus-pinger \
 	"${log_args[@]}" \
+	${conn_type} \
+	--msg-bus-router-address /tmp/sudoku_t \
 	--ping-count 100k \
 	--limit-count 25 \
-	${conn_type} \
 	& pids+=($!)
 
 for pid in ${pids[@]}
